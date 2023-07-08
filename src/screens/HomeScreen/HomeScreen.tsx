cb43cd8df516f5cc35b1416b8fa20a7502ca643b
HoCG <rhwkd5012@naver.com>
2023-07-08 11:01:51 +0900

    🚧 wip: navigator 구축을 위해 Screen 임시추가

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>홈</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
