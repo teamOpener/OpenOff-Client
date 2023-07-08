e9b7c89b773845b8ede016de53a59b887433aefb
HoCG <rhwkd5012@naver.com>
2023-07-08 11:00:03 +0900

    💄 style: enum 사용시 eslint 'declared in upper scope' 검사 방지

---
+++ src/constants/menu.ts
@@ -0,0 +1,19 @@
+export const enum StackMenu {
+  BottomTabNavigator = 'BottomTabNavigator',
+}
+
+export const enum BottomTabMenu {
+  Home = 'Home',
+  My = 'My',
+}
+
+export const enum AuthorizeMenu {
+  Login = 'Login',
+  AgreeToTerm = 'AgreeToTerm',
+  PhoneCertification = 'PhoneCertification',
+  EmailPassword = 'EmailPassword',
+  NickName = 'NickName',
+  UserInfo = 'UserInfo',
+  InterestField = 'InterestField',
+  JoinComplete = 'JoinComplete',
+}
