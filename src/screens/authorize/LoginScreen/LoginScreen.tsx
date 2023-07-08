ba5ac398b0a0b081b0640c09508f5d33ed7a2398
HoCG <rhwkd5012@naver.com>
2023-07-08 11:01:51 +0900

    🚧 wip: navigator 구축을 위해 Screen 임시추가

---
+++ src/screens/authorize/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LoginPage = () => {
+  return (
+    <View>
+      <Text>로그인 페이지</Text>
+    </View>
+  );
+};
+
+export default LoginPage;
