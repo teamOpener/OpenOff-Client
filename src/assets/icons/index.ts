7a3aa03ba0c319f7b561a6d6fd19aff258702246
Yeonju Jo <yjj0287@gmail.com>
2023-07-08 19:42:56 +0800

    ✨ feat: design system icon 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,16 @@
+export { default as IconArrowLeft } from './IconArrowLeft.svg';
+export { default as IconBookmark } from './IconBookmark.svg';
+export { default as IconCheck } from './IconCheck.svg';
+export { default as IconComment } from './IconComment.svg';
+export { default as IconCongrates } from './IconCongrates.svg';
+export { default as IconExitCircle } from './IconExitCircle.svg';
+export { default as IconHeart } from './IconHeart.svg';
+export { default as IconHome } from './IconHome.svg';
+export { default as IconMap } from './IconMap.svg';
+export { default as IconSearch } from './IconSearch.svg';
+export { default as IconSend } from './IconSend.svg';
+export { default as IconSetting } from './IconSetting.svg';
+export { default as IconShare } from './IconShare.svg';
+export { default as IconTicket } from './IconTicket.svg';
+export { default as IconUser } from './IconUser.svg';
+export { default as IconWarning } from './IconWarning.svg';
