06d841eb864b86bb22aea7a1405897fddea3daad
HoCG <rhwkd5012@naver.com>
2023-07-10 00:20:28 +0900

    ✨ feat: 닉네임 설정 화면 구현

---
+++ src/screens/authorize/NickNameScreen/NickNameScreen.style.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+import { colors } from 'styles/theme';
+
+const nickNameScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+    backgroundColor: colors.background,
+    alignItems: 'center',
+  },
+  title: {
+    color: colors.white,
+    fontSize: 25,
+    height: 35,
+    width: 350,
+  },
+  titleContainer: {
+    marginVertical: 40,
+  },
+});
+
+export default nickNameScreenStyles;
