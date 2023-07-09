d2f2add2bd46fdbe64fe6be866a80dab9c83bdeb
HoCG <rhwkd5012@naver.com>
2023-07-10 00:10:30 +0900

    ✨ feat: 분야버튼 \& 분야버튼 그룹 구현

---
+++ src/components/authorize/groups/FieldButtonGroup/FieldButtonGroup.tsx
@@ -0,0 +1,41 @@
+import FieldButton from 'components/authorize/buttons/FieldButton/FieldButton';
+import { Dispatch, SetStateAction } from 'react';
+import { Text, View } from 'react-native';
+import { Field } from 'types/apps/group';
+import fieldButtonGroupStyles from './FieldButtonGroup.style';
+
+interface Props {
+  fields: Field[];
+  setFields: Dispatch<SetStateAction<Field[]>>;
+  computedCount: number;
+}
+
+const FieldButtonGroup = ({ fields, setFields, computedCount }: Props) => {
+  const handleFieldActive = (field: Field) => {
+    if (computedCount >= 3 && !field.isActive) return;
+    setFields(
+      fields.map((mappingField) => {
+        if (mappingField.value === field.value) {
+          Object.assign(mappingField, { isActive: !mappingField.isActive });
+        }
+        return mappingField;
+      }),
+    );
+  };
+  return (
+    <View style={fieldButtonGroupStyles.container}>
+      <Text style={fieldButtonGroupStyles.title}>최대 3가지 선택 가능</Text>
+      <View style={fieldButtonGroupStyles.fieldContainer}>
+        {fields.map((field) => (
+          <FieldButton
+            key={field.value}
+            field={field}
+            handlePress={handleFieldActive}
+          />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default FieldButtonGroup;
