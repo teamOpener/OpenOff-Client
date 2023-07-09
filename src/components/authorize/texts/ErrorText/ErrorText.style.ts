fbcd647e3f0af7e6d841e4063bcd562fa864def9
HoCG <rhwkd5012@naver.com>
2023-07-10 00:14:38 +0900

    ✨ feat: 공통으로 사용되는 Error Text 추가

---
+++ src/components/authorize/texts/ErrorText/ErrorText.style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { colors } from 'styles/theme';
+
+const errorTextStyles = StyleSheet.create({
+  text: {
+    color: colors.error,
+    height: 20,
+    width: '100%',
+  },
+});
+
+export default errorTextStyles;
