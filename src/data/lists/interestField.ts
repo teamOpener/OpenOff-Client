c0ea7055d14ab04196bcfd126bbcaf9952bfdb4c
HoCG <rhwkd5012@naver.com>
2023-07-10 00:15:20 +0900

    ✨ feat: 관심 분야에 사용될 static 데이터값 추가

---
+++ src/data/lists/interestField.ts
@@ -0,0 +1,36 @@
+import { Field } from 'types/apps/group';
+
+const interestField: Field[] = [
+  {
+    value: 'lecture',
+    isActive: false,
+    label: '강연/세미나/컨퍼런스',
+  },
+  {
+    value: 'exhibition',
+    isActive: false,
+    label: '전시/박람회',
+  },
+  {
+    value: 'performance',
+    isActive: false,
+    label: '공연',
+  },
+  {
+    value: 'exercise',
+    isActive: false,
+    label: '운동/액티비티',
+  },
+  {
+    value: 'food',
+    isActive: false,
+    label: '푸드/드링크',
+  },
+  {
+    value: 'party',
+    isActive: false,
+    label: '파티/페스티벌',
+  },
+];
+
+export default interestField;
