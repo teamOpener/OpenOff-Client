18f47de0c70b1efe6a20b7362a5746ea30bde915
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-11 18:36:54 +0900

    ✨ feat: bottomTabNavigator에 이벤트맵 추가

--- src/navigators/BottomTabNavigator.tsx
+++ src/navigators/BottomTabNavigator.tsx
@@ -1,17 +1,57 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'components/common/Icon/Icon';
 import { BottomTabMenu } from 'constants/menu';
+import EventMapScreen from 'screens/EventMapScreen/EventMapScreen';
 import HomeScreen from 'screens/HomeScreen/HomeScreen';
+import textStyles from 'styles/textStyles';
+import { colors } from 'styles/theme';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName={BottomTabMenu.Home}
+      screenOptions={{
+        tabBarActiveTintColor: colors.white,
+        tabBarInactiveTintColor: colors.darkGrey,
+        tabBarStyle: {
+          backgroundColor: colors.background,
+        },
+      }}
+    >
       <Tab.Screen
         name={BottomTabMenu.Home}
         component={HomeScreen}
         options={{
+          headerShown: true,
           tabBarLabel: '홈',
+          tabBarLabelStyle: {
+            ...textStyles.caption,
+          },
+          tabBarIcon: ({ focused }) =>
+            Icon({
+              name: 'IconHome',
+              fill: focused ? 'white' : 'darkGrey',
+              size: 20,
+            }),
+        }}
+      />
+      <Tab.Screen
+        name={BottomTabMenu.EventMap}
+        component={EventMapScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: '이벤트맵',
+          tabBarLabelStyle: {
+            ...textStyles.caption,
+          },
+          tabBarIcon: ({ focused }) =>
+            Icon({
+              name: 'IconMap',
+              fill: focused ? 'white' : 'darkGrey',
+              size: 20,
+            }),
         }}
       />
     </Tab.Navigator>
