5aed9bb1c727e2751392a7af08b82088973daa2d
HoCG <rhwkd5012@naver.com>
2023-07-11 23:48:02 +0900

    ✨ feat: app.tsx에 GestureHandlerRootView 추가

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@
 
 // 스토리북 실행을 원한다면 위에 코드 주석 해제, 아래 코드 주석처리
 // 서비스 실행을 원한다면 아래 코드 주석 해제, 위에 코드 주석처리
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthorizeNavigator from 'navigators/AuthorizeNavigator';
 import Navigator from 'navigators/Navigator';
@@ -12,9 +12,15 @@ import { useState } from 'react';
 const App = () => {
   const [isLogin, setIsLogin] = useState<boolean>(false);
   return (
-    <NavigationContainer>
-      {isLogin ? <Navigator /> : <AuthorizeNavigator setIsLogin={setIsLogin} />}
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        {isLogin ? (
+          <Navigator />
+        ) : (
+          <AuthorizeNavigator setIsLogin={setIsLogin} />
+        )}
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
