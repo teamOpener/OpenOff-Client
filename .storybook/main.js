ed430f62b5141218e8b8036d619f871d9bf5e213
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-11 18:18:56 +0900

    💄 style: storybook이 components가 아닌 src를 바라보도록 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/components/**/**/*.stories.?(ts|tsx|js|jsx)'],
+  stories: ['../src/**/*.stories.?(ts|tsx|js|jsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
