0fa47ab3b49d6b2eff64e4dbbc60f9503fe1083b
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-12 18:06:35 +0900

    ✨ feat: 캘린더 카드 구현

---
+++ src/components/eventMap/cards/CalendarCard/CalendarCard.style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const calendarCardStyles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    width: '100%',
+  },
+});
+
+export default calendarCardStyles;
