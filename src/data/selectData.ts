056940e9665aa7dbe4c5d0ebe9af32d2fff2c7d6
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-13 13:16:11 +0900

    ✨ feat: 셀렉트 박스에 사용될 static한 값 추가

---
+++ src/data/selectData.ts
@@ -0,0 +1,48 @@
+import Option from 'types/apps/selectbox';
+
+const payOptions: Option[] = [
+  {
+    label: '전체',
+    value: 'all',
+  },
+  {
+    label: '유료',
+    value: 'pay',
+  },
+  {
+    label: '무료',
+    value: 'free',
+  },
+];
+
+const participantOptions: Option[] = [
+  {
+    label: '20인 미만',
+    value: '20',
+  },
+  {
+    label: '20인 ~ 50인',
+    value: '20to50',
+  },
+  {
+    label: '50인 ~ 100인',
+    value: '50to100',
+  },
+  {
+    label: '100인 이상',
+    value: '100',
+  },
+];
+
+const applicationAbleOption: Option[] = [
+  {
+    label: '신청중',
+    value: 'applying',
+  },
+  {
+    label: '신청마감',
+    value: 'deadline',
+  },
+];
+
+export { applicationAbleOption, participantOptions, payOptions };
