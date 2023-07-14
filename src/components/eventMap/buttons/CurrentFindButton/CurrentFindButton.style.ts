ea4cde1c013876ca2564d3f88c314454000848cf
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-14 18:40:03 +0900

    ✨ feat: 현재 화면으로 이벤트 검색 버튼 구현

---
+++ src/components/eventMap/buttons/CurrentFindButton/CurrentFindButton.style.ts
@@ -0,0 +1,22 @@
+import { Dimensions, StyleSheet } from 'react-native';
+import { colors } from 'styles/theme';
+
+const currentFindButtonStyles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    zIndex: 3,
+    top: 50,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: colors.main,
+    left: Dimensions.get('window').width / 2 - 100,
+    width: 200,
+    borderRadius: 17,
+    height: 33,
+  },
+});
+
+export default currentFindButtonStyles;
