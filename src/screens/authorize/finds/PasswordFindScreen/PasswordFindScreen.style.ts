90a24315cf0492ca321169dac8991042c92f0c87
HoCG <rhwkd5012@naver.com>
2023-07-16 01:58:38 +0900

    ✨ feat: 비밀번호 찾기 화면 구현

---
+++ src/screens/authorize/finds/PasswordFindScreen/PasswordFindScreen.style.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const passwordFindScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 10,
+    marginTop: 20,
+  },
+});
+
+export default passwordFindScreenStyles;
