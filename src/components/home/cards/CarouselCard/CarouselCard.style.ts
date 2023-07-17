708e38ec0b72839d61fe99e3ec85f28f3c002ea4
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-17 17:13:19 +0900

    ✨ feat: 홈에 carousel 1차 구현

---
+++ src/components/home/cards/CarouselCard/CarouselCard.style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const carouselCardStyle = StyleSheet.create({
+  container: {
+    backgroundColor: 'floralwhite',
+    borderRadius: 5,
+    height: 250,
+    padding: 50,
+  },
+  titleText: {
+    color: 'black',
+    fontSize: 30,
+  },
+});
+
+export default carouselCardStyle;
---
+++ src/types/apps/carousel.ts
@@ -0,0 +1,4 @@
+export interface CarouselElement {
+  title: string;
+  text: string;
+}
