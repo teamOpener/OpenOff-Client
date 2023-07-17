84eb880eebbc3992926c0f8fa570d3150d18ceec
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-17 15:04:11 +0900

    ✨ feat: 셀렉트 상세 버튼 구현

---
+++ src/components/eventMap/buttons/SelectDetailButton/SelectDetailButton.style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const selectDetailButtonStyles = StyleSheet.create({
+  container: {
+    borderRadius: 17,
+    borderWidth: 1,
+    paddingHorizontal: 17,
+    paddingVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+    marginTop: 10,
+  },
+});
+
+export default selectDetailButtonStyles;
