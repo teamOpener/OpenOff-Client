5211439b8f8ba25cfa9de6dcdf87f49454e0db90
HoCG <rhwkd5012@naver.com>
2023-07-17 23:03:46 +0900

    ✨ feat: 이벤트 카드 \& 이벤트 카드 그룹 구현

---
+++ src/components/home/groups/EventCardGroup/EventCardGroup.style.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const eventCardGroupStyles = StyleSheet.create({
+  container: {
+    marginBottom: 50,
+  },
+});
+
+export default eventCardGroupStyles;
