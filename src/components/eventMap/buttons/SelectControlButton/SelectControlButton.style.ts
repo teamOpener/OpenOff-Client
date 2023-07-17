369f6c179b2041bd3ef5cd89f98cd613404e6ce5
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-17 15:11:12 +0900

    ✨ feat: 셀렉트 디테일을 조작가능한 버튼 구현

---
+++ src/components/eventMap/buttons/SelectControlButton/SelectControlButton.style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const selectControlButtonStyles = StyleSheet.create({
+  container: {
+    width: 170,
+    height: 54,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 27.5,
+    borderWidth: 1,
+    marginHorizontal: 5,
+  },
+});
+
+export default selectControlButtonStyles;
