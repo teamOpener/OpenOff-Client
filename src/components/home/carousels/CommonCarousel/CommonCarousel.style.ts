f87015e4aad5cd70a9b797fd4e5cf8c43fed4f23
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-19 10:31:20 +0900

    🚚 rename: commonCarousel을 advertisementCarousel로 명칭변경

--- src/components/home/carousels/CommonCarousel/CommonCarousel.style.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const commonCarouselStyles = StyleSheet.create({
-  container: {
-    marginTop: 20,
-  },
-});
-
-export default commonCarouselStyles;
--- src/types/apps/carousel.ts
+++
@@ -1,4 +0,0 @@
-export interface CarouselElement {
-  title: string;
-  text: string;
-}
