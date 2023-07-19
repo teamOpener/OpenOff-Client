80bbfd26e7c9d502efdbdfcae21be1fd42dd35e0
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-19 16:07:25 +0900

    ✨ feat: linear-gradient를 광고 케러셀에 적용

--- src/components/home/cards/CarouselCard/CarouselCard.tsx
+++ src/components/home/cards/CarouselCard/CarouselCard.tsx
@@ -1,6 +1,7 @@
 import Text from 'components/common/Text/Text';
 import field from 'data/lists/field';
 import { ImageBackground, TouchableOpacity, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { Advertisement } from 'types/apps/advertisement';
 import carouselCardStyle from './CarouselCard.style';
 
@@ -22,24 +23,27 @@ const CarouselCard = <T extends Advertisement>({
         source={{ uri: item.image }}
         resizeMode="cover"
       >
-        <View style={carouselCardStyle.carouselInfo}>
-          <Text variant="h3" color="white">
-            {item.title}
-          </Text>
-          <View style={carouselCardStyle.fieldContainer}>
-            <Text>
-              {
-                field.find((fieldElement) => fieldElement.value === item.field)
-                  ?.label
-              }
+        <LinearGradient colors={['#19191900', '#191919AB', '#191919E5']}>
+          <View style={carouselCardStyle.carouselInfo}>
+            <Text variant="h3" color="white">
+              {item.title}
             </Text>
+            <View style={carouselCardStyle.fieldContainer}>
+              <Text>
+                {
+                  field.find(
+                    (fieldElement) => fieldElement.value === item.field,
+                  )?.label
+                }
+              </Text>
+            </View>
+            <View style={carouselCardStyle.pageInfo}>
+              <Text variant="body3" color="white">
+                {index + 1}/{length}
+              </Text>
+            </View>
           </View>
-          <View style={carouselCardStyle.pageInfo}>
-            <Text variant="body3" color="white">
-              {index + 1}/{length}
-            </Text>
-          </View>
-        </View>
+        </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
   );
