adf94a637362e42b032a294087d54a9869e9da22
Yeonju Jo <yjj0287@gmail.com>
2023-07-22 23:33:30 +0900

    ✨ feat: android splash screen 추가

--- android/app/src/main/java/com/opener/openoff/MainActivity.java
+++ android/app/src/main/java/com/opener/openoff/MainActivity.java
@@ -1,9 +1,11 @@
 package com.opener.openoff;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,6 +17,12 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "OpenOff";
   }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme, true);
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
