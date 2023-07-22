623f7ac501106fd0f08e30bcaa23eff049c9abb4
Yeonju Jo <yjj0287@gmail.com>
2023-07-22 18:14:56 +0900

    ✨ feat: ios splash screen 추가

--- App.tsx
+++ App.tsx
@@ -5,12 +5,19 @@
 // 서비스 실행을 원한다면 아래 코드 주석 해제, 위에 코드 주석처리
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import AuthorizeNavigator from 'navigators/AuthorizeNavigator';
 import Navigator from 'navigators/Navigator';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const App = () => {
   const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => SplashScreen.hide(), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
