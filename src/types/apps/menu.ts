38fa8d24a2004a89cc274fda57f5410188a21b2b
Yeonju Jo <yjj0287@gmail.com>
2023-07-23 04:05:38 +0900

    ✨ feat: navigation custom hook 제작

--- src/types/apps/menu.ts
+++ src/types/apps/menu.ts
@@ -1,4 +1,10 @@
 import { Coordinate } from 'types/event';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import {
+  CompositeNavigationProp,
+  NavigatorScreenParams,
+} from '@react-navigation/native';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { Field } from './group';
 
 export type AuthStackParamList = {
@@ -14,11 +20,30 @@ export type AuthStackParamList = {
   InterestField: undefined;
 };
 
+export type BottomTabParamList = {
+  Home: undefined;
+  EventMap: undefined;
+  UserEvent: undefined;
+  User: undefined;
+};
+
+export type BottomTabNavigationScreenParams =
+  NavigatorScreenParams<BottomTabParamList>;
+
 export type RootStackParamList = {
-  BottomTabNavigator: undefined;
+  BottomTabNavigator: BottomTabNavigationScreenParams;
   DatePick: undefined;
   FieldEventMap: {
     field: Field;
     coordinate: Coordinate;
   };
+  AddEvent: undefined;
 };
+
+export type RootStackNavigationProp =
+  NativeStackNavigationProp<RootStackParamList>;
+
+export type MainTabNavigationProp = CompositeNavigationProp<
+  RootStackNavigationProp,
+  BottomTabNavigationProp<BottomTabParamList>
+>;
