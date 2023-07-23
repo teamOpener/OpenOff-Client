96c818456daf575962a9d019a8a1c58c08b2a853
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-23 21:11:26 +0900

    ✨ feat: iOS 환경에서도 android처럼 키보드위에 버튼이 오도록 기능구현

---
+++ src/components/authorize/covers/ScreenCover/ScreenCover.style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const screenCoverStyles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+    padding: 10,
+  },
+});
+
+export default screenCoverStyles;
---
+++ src/components/authorize/covers/ScreenCover/ScreenCover.tsx
@@ -0,0 +1,60 @@
+import AuthorizeFlowButton from 'components/authorize/buttons/AuthorizeFlowButton/AuthorizeFlowButton';
+import { ReactNode, SetStateAction, useEffect, useState } from 'react';
+import {
+  KeyboardAvoidingView,
+  NativeModules,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import screenCoverStyles from './ScreenCover.style';
+
+interface Props {
+  children?: ReactNode;
+  authorizeButton: {
+    handlePress: () => void;
+    label: string;
+    isActive: boolean;
+  };
+}
+
+const ScreenCover = ({ children, authorizeButton }: Props) => {
+  const [statusBarHeight, setStatusBarHeight] = useState<number>(0);
+  const { StatusBarManager } = NativeModules;
+  useEffect(() => {
+    if (Platform.OS === 'ios')
+      StatusBarManager.getHeight(
+        (statusBarFrameData: { height: SetStateAction<number> }) => {
+          setStatusBarHeight(statusBarFrameData.height);
+        },
+      );
+  });
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={statusBarHeight + 40}
+      style={screenCoverStyles.scrollContainer}
+    >
+      {Platform.OS === 'ios' ? (
+        <ScrollView>{children}</ScrollView>
+      ) : (
+        <>
+          {children}
+          <AuthorizeFlowButton
+            handlePress={authorizeButton.handlePress}
+            label={authorizeButton.label}
+            isActive={authorizeButton.isActive}
+          />
+        </>
+      )}
+      {Platform.OS === 'ios' && (
+        <AuthorizeFlowButton
+          handlePress={authorizeButton.handlePress}
+          label={authorizeButton.label}
+          isActive={authorizeButton.isActive}
+        />
+      )}
+    </KeyboardAvoidingView>
+  );
+};
+
+export default ScreenCover;
