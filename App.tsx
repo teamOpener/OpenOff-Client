2aa92be7fddf9c89e55b148402009b01dd654c01
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-23 23:56:44 +0900

    ✨ feat: app.tsx상에 SafeAreaView 적용

--- App.tsx
+++ App.tsx
@@ -3,12 +3,24 @@
 
 // 스토리북 실행을 원한다면 위에 코드 주석 해제, 아래 코드 주석처리
 // 서비스 실행을 원한다면 아래 코드 주석 해제, 위에 코드 주석처리
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import SplashScreen from 'react-native-splash-screen';
 import AuthorizeNavigator from 'navigators/AuthorizeNavigator';
 import Navigator from 'navigators/Navigator';
 import { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
+import { colors } from 'styles/theme';
+
+const appStyles = StyleSheet.create({
+  gestureContainer: {
+    flex: 1,
+  },
+  safeAreaContainer: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
 
 const App = () => {
   const [isLogin, setIsLogin] = useState<boolean>(false);
@@ -19,15 +31,17 @@ const App = () => {
   }, []);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        {isLogin ? (
-          <Navigator />
-        ) : (
-          <AuthorizeNavigator setIsLogin={setIsLogin} />
-        )}
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <SafeAreaView style={appStyles.safeAreaContainer}>
+      <GestureHandlerRootView style={appStyles.gestureContainer}>
+        <NavigationContainer>
+          {isLogin ? (
+            <Navigator />
+          ) : (
+            <AuthorizeNavigator setIsLogin={setIsLogin} />
+          )}
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 };
 
