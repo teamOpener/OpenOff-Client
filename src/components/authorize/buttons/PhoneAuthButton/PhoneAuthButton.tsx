3a808fa5f48103dd637c73735b9c8fea66cf7606
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-24 19:14:57 +0900

    💅 design: 핸드폰 인증 버튼 레이아웃 개선

--- src/components/authorize/buttons/PhoneAuthButton/PhoneAuthButton.tsx
+++ src/components/authorize/buttons/PhoneAuthButton/PhoneAuthButton.tsx
@@ -1,4 +1,3 @@
-import { View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Text from '../../../common/Text/Text';
 import phoneAuthButtonStyles from './PhoneAuthButton.style';
@@ -15,20 +14,18 @@ const PhoneAuthButton = ({ label, active, handlePress }: Props) => {
     handlePress();
   };
   return (
-    <View style={phoneAuthButtonStyles.container}>
-      <TouchableOpacity
-        style={
-          active
-            ? phoneAuthButtonStyles.activeButton
-            : phoneAuthButtonStyles.nonActiveButton
-        }
-        onPress={hadleAuthPress}
-      >
-        <Text variant="caption" color={active ? 'white' : 'grey'}>
-          {label}
-        </Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      style={
+        active
+          ? phoneAuthButtonStyles.activeButton
+          : phoneAuthButtonStyles.nonActiveButton
+      }
+      onPress={hadleAuthPress}
+    >
+      <Text variant="caption" color={active ? 'white' : 'grey'}>
+        {label}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
