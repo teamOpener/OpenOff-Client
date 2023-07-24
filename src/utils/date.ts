87fa688221435d098d34771d9969b60f0b12f44e
Yeonju Jo <yjj0287@gmail.com>
2023-07-25 01:49:27 +0900

    ✨ feat: 이벤트 일시 표기를 위한 formatter util 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -11,4 +11,17 @@ const dateFormatter = (date: Date) => {
   return `${date.getFullYear()}-${calcDay(month)}-${calcDay(day)}`;
 };
 
-export { calcDay, dateFormatter };
+const formatDateTime = (date: Date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const time = date.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+
+  return `${year}.${month}.${day} ${time}`;
+};
+
+export { calcDay, dateFormatter, formatDateTime };
