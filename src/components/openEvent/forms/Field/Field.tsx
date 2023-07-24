6dce18c91b53852b55c3e236cd99c0968b0cfee0
Yeonju Jo <yjj0287@gmail.com>
2023-07-25 01:58:59 +0900

    ✨ feat: field form data 추가

---
+++ src/components/openEvent/forms/Field/Field.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { OpenEvent } from 'components/openEvent';
+import Spacing from 'components/common/Spacing/Spacing';
+import MENT_OPEN_EVENT from 'constants/openEvent';
+import StatusType from 'constants/status';
+import { Field as FieldType } from 'types/apps/group';
+import field from 'data/lists/field';
+import { useOpenEventStore } from 'stores/OpenEventStore';
+import useField from 'hooks/openEvent/useField';
+
+const Field = () => {
+  const {
+    openEvent,
+    setOpenEvent,
+    openEventErrorMessage,
+    setOpenEventErrorMessage,
+  } = useOpenEventStore();
+
+  const [eventField, setEventField] = useState<FieldType[]>(field);
+  const { toggleFieldIsActive, getActiveFieldCodes } = useField({
+    eventField,
+  });
+
+  const handlePress = (f: FieldType): void => {
+    const updatedFields = toggleFieldIsActive(eventField, f);
+    setEventField(updatedFields);
+    setOpenEventErrorMessage({ ...openEventErrorMessage, field: null });
+  };
+
+  useEffect(() => {
+    setOpenEvent({ ...openEvent, field: getActiveFieldCodes() });
+  }, [eventField]);
+
+  return (
+    <View>
+      <OpenEvent.Label content="이벤트 분야" />
+      <OpenEvent.HelpText
+        content={
+          openEventErrorMessage.field === null
+            ? MENT_OPEN_EVENT.FIELD_HELP_TEXT
+            : openEventErrorMessage.field
+        }
+        status={
+          openEventErrorMessage.field === null
+            ? StatusType.default
+            : StatusType.error
+        }
+      />
+      <Spacing height={15} />
+
+      <OpenEvent.TagGroup>
+        {eventField.map((f) => (
+          <OpenEvent.Tag
+            key={f.value}
+            label={f.label}
+            isSelected={f.isActive}
+            handlePress={() => handlePress(f)}
+          />
+        ))}
+      </OpenEvent.TagGroup>
+    </View>
+  );
+};
+
+export default Field;
