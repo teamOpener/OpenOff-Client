64e1f59e233b197f4f26343d556e6195f60d4ca5
Yeonju Jo <yjj0287@gmail.com>
2023-07-25 02:00:38 +0900

    ✨ feat: 신청 기간 폼 컴포넌트 추가

---
+++ src/components/openEvent/forms/ApplicationPeriod/ApplicationPeriod.tsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import Text from 'components/common/Text/Text';
+import { OpenEvent } from 'components/openEvent';
+import { useOpenEventStore } from 'stores/OpenEventStore';
+import { formatDateTime } from 'utils/date';
+import { HelpText } from 'components/openEvent/OpenEvent';
+import StatusType from 'constants/status';
+import applicationPeriodStyles from './ApplicationPeriod.style';
+
+const ApplicationPeriod = () => {
+  const {
+    openEvent,
+    openEventErrorMessage,
+    setOpenEvent,
+    setOpenEventErrorMessage,
+  } = useOpenEventStore();
+
+  const [startDate, setStartDate] = useState<Date>(
+    openEvent.applicationStartDate === null
+      ? new Date()
+      : new Date(openEvent.applicationStartDate),
+  );
+  const [endDate, setEndDate] = useState<Date>(
+    openEvent.applicationEndDate === null
+      ? new Date()
+      : new Date(openEvent.applicationEndDate),
+  );
+
+  const hasError = openEventErrorMessage.applicationPeriod !== null;
+
+  useEffect(() => {
+    if (hasError) {
+      setOpenEventErrorMessage({
+        ...openEventErrorMessage,
+        applicationPeriod: null,
+      });
+    }
+
+    setOpenEvent({
+      ...openEvent,
+      applicationStartDate: formatDateTime(startDate),
+    });
+  }, [startDate]);
+
+  useEffect(() => {
+    if (hasError) {
+      setOpenEventErrorMessage({
+        ...openEventErrorMessage,
+        applicationPeriod: null,
+      });
+    }
+
+    setOpenEvent({
+      ...openEvent,
+      applicationEndDate: formatDateTime(endDate),
+    });
+  }, [endDate]);
+
+  return (
+    <View>
+      <OpenEvent.Label content="이벤트 신청 기간" />
+
+      <View style={applicationPeriodStyles.inputContainer}>
+        <View style={applicationPeriodStyles.dateTimePickerContainer}>
+          <View style={applicationPeriodStyles.dateTimePickerWrapper}>
+            <OpenEvent.DateTimePicker
+              date={startDate}
+              setDate={setStartDate}
+              isEmpty={openEvent.applicationStartDate === null}
+              hasError={hasError}
+            />
+            <Text>부터</Text>
+          </View>
+
+          <View style={applicationPeriodStyles.dateTimePickerWrapper}>
+            <OpenEvent.DateTimePicker
+              date={endDate}
+              setDate={setEndDate}
+              disabled={openEvent.applicationStartDate === null}
+              isEmpty={openEvent.applicationEndDate === null}
+              minimumDate={startDate}
+              hasError={hasError}
+            />
+            <Text>까지</Text>
+          </View>
+        </View>
+
+        {hasError && (
+          <HelpText
+            status={StatusType.error}
+            content={openEventErrorMessage.applicationPeriod ?? ''}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default ApplicationPeriod;
