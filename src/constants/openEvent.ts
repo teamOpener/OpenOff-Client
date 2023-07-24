bd93212707781068d6702a59f5693bf80da49b9d
Yeonju Jo <yjj0287@gmail.com>
2023-07-25 01:48:23 +0900

    ✨ feat: 이벤트 개설 페이지 상수 추가

---
+++ src/constants/openEvent.ts
@@ -0,0 +1,26 @@
+const MENT_OPEN_EVENT = Object.freeze({
+  INFO: '이벤트 정보를 입력해주세요.',
+  FIELD_HELP_TEXT: '여러 개의 장르 선택이 가능하고, 장르 검색시 적용됩니다.',
+  DATE_PICKER_PLACEHOLDER: '날짜를 선택해주세요.',
+  DATE_PICKER_TITLE: '날짜를 선택해주세요.',
+  TIME_PICKER_TITLE: '시간을 선택해주세요.',
+  ADDRESS_PLACEHOLDER: '주소를 검색해주세요',
+  ADDRESS_DETAIL_PLACEHOLDER: '상세 주소를 입력해주세요',
+  DESCRIPTION_PLACEHOLDER:
+    '다음과 같은 정보를 입력해주세요.\n\n-이벤트 상세 설명 및 소개\n-나이 제한 (신분증 지참)\n-이벤트 준비물\n-유료 이벤트의 경우 현장 결제 안내',
+  IMAGE_UPLOAD_HELP_TEXT: '최소 1장의 사진을 등록해주세요.',
+  ADDITIONAL_PLACEHOLDER: '추가로 수집할 정보를 입력해주세요',
+
+  ERROR_FIELD: '최소 1개 이상의 분야를 선택해주세요.',
+  ERROR_TITLE: '이벤트 제목을 입력해주세요.',
+  ERROR_APPLICATION_DATE: '이벤트 신청 기간을 설정해주세요.',
+  ERROR_DATE: '최소 1개 이상의 이벤트 일시를 등록해주세요.',
+  ERROR_ADDRESS: '이벤트 장소를 등록해주세요.',
+  ERROR_RECRUITMENT: '모집 인원를 입력해주세요.',
+  ERROR_DESCRIPTION: '이벤트 상세 내용을 입력해주세요.',
+  ERROR_HOST_NAME: '업체(주최자)명을 입력해주세요.',
+  ERROR_HOST_PHONE: '전화번호를 입력해주세요.',
+  ERROR_HOST_EMAIL: '이메일을 입력해주세요.',
+});
+
+export default MENT_OPEN_EVENT;
---
+++ src/constants/status.ts
@@ -0,0 +1,7 @@
+const enum StatusType {
+  default = 'default',
+  correct = 'correct',
+  error = 'error',
+}
+
+export default StatusType;
