cc2c0583d6fb01b0ad79f2903da6dbc183eb83ea
Yeonju Jo <yjj0287@gmail.com>
2023-07-25 02:00:38 +0900

    ✨ feat: 신청 기간 폼 컴포넌트 추가

---
+++ src/components/openEvent/forms/ApplicationPeriod/ApplicationPeriod.style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const applicationPeriodStyles = StyleSheet.create({
+  inputContainer: {
+    gap: 5,
+  },
+  dateTimePickerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+  },
+  dateTimePickerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+});
+
+export default applicationPeriodStyles;
