9cf2205235ed5a2be99a7a96e1405f4a351609e1
Yeonju Jo <yjj0287@gmail.com>
2023-07-25 17:12:01 +0900

    ✨ feat: 이벤트 개설 페이지 폼 추가

--- src/screens/home/OpenEventScreen/OpenEventScreen.tsx
+++ src/screens/home/OpenEventScreen/OpenEventScreen.tsx
@@ -1,11 +1,72 @@
-import { View } from 'react-native';
-import Text from 'components/common/Text/Text';
-import AddEventScreenStyles from './OpenEventScreen.style';
+import { useEffect } from 'react';
+import { ScrollView, View } from 'react-native';
+import { useOpenEventStore } from 'stores/OpenEventStore';
+import MENT_OPEN_EVENT from 'constants/openEvent';
+import Spacing from 'components/common/Spacing/Spacing';
+import Divider from 'components/common/Divider/Divider';
+import FixedButton from 'components/common/FixedButton/FixedButton';
+import { OpenEvent, OpenEventForm } from 'components/openEvent';
+import useOpenEventValidator from 'hooks/openEvent/useOpenEventValidator';
+import openEventScreenStyles from './OpenEventScreen.style';
 
 const OpenEventScreen = () => {
+  const { init, openEvent, setOpenEventErrorMessage } = useOpenEventStore();
+
+  const { hasError, errorMessage } = useOpenEventValidator({ openEvent });
+
+  const handlePress = () => {
+    if (hasError) {
+      setOpenEventErrorMessage(errorMessage);
+      return;
+    }
+    // TODO: submit -> success/fail
+    console.log(openEvent);
+  };
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
   return (
-    <View style={AddEventScreenStyles.container}>
-      <Text>OpenEventScreen</Text>
+    <View style={openEventScreenStyles.wrapper}>
+      <ScrollView
+        style={openEventScreenStyles.container}
+        contentContainerStyle={openEventScreenStyles.containerContent}
+      >
+        <OpenEvent.Header title={MENT_OPEN_EVENT.INFO} />
+
+        <OpenEventForm.Field />
+        <OpenEventForm.Title />
+        <OpenEventForm.ApplicationPeriod />
+        <OpenEventForm.Period />
+        {/* TODO */}
+        {/* <OpenEventForm.Address /> */}
+        {/* <OpenEventForm.Cost /> */}
+        {/* <OpenEventForm.RecruitmentNumber /> */}
+        {/* <OpenEventForm.Description /> */}
+        {/* <OpenEventForm.UploadImage /> */}
+        {/* <OpenEventForm.AdditionalInfo /> */}
+
+        <Divider height={1} color="darkGrey" />
+
+        <OpenEvent.Header title={MENT_OPEN_EVENT.INFO} />
+        {/* TODO */}
+        {/* <OpenEventForm.HostName />
+        <OpenEventForm.HostPhoneNumber />
+        <OpenEventForm.HostEmail /> */}
+
+        <Divider height={1} color="darkGrey" />
+
+        <OpenEvent.Header title="개인정보 수집 및 이용" />
+        {/* TODO */}
+
+        <OpenEvent.Header title="유의사항" />
+        {/* TODO */}
+
+        <Spacing height={156} />
+      </ScrollView>
+
+      <FixedButton label="개설하기" onPress={handlePress} />
     </View>
   );
 };
