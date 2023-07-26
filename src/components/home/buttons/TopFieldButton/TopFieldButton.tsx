caa136cd2c58c2fa66ebb79a1c859012f2e8aaad
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-26 13:30:48 +0900

    ✨ feat: 상단 분야버튼 \& 분야버튼 그룹 구현

---
+++ src/components/home/buttons/TopFieldButton/TopFieldButton.tsx
@@ -0,0 +1,34 @@
+import Text from 'components/common/Text/Text';
+import { Pressable, View } from 'react-native';
+import { colors } from 'styles/theme';
+import { Field } from 'types/apps/group';
+import topFieldButtonStyles from './TopFieldButton.style';
+
+interface Props {
+  field: Field;
+  handlePress: (value: string) => void;
+}
+
+const TopFieldButton = ({ field, handlePress }: Props) => {
+  return (
+    <Pressable
+      style={topFieldButtonStyles.container}
+      onPress={() => handlePress(field.value)}
+    >
+      <Text
+        style={topFieldButtonStyles.fieldText}
+        color={field.isActive ? 'white' : 'grey'}
+      >
+        {field.label}
+      </Text>
+      <View
+        style={{
+          ...topFieldButtonStyles.delimiter,
+          backgroundColor: field.isActive ? colors.main : colors.background,
+        }}
+      />
+    </Pressable>
+  );
+};
+
+export default TopFieldButton;
