1a996394b7d29c82fdf38bf76e2742e8def89549
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-26 13:30:48 +0900

    ✨ feat: 상단 분야버튼 \& 분야버튼 그룹 구현

---
+++ src/components/home/buttons/TopFieldButton/TopFieldButton.style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const topFieldButtonStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 36,
+  },
+  fieldText: {
+    marginHorizontal: 6,
+  },
+  delimiter: {
+    height: 5,
+    width: '100%',
+    marginTop: 5,
+  },
+});
+
+export default topFieldButtonStyles;
---
+++ src/components/home/groups/TopFieldButtonGroup/TopFieldButtonGroup.tsx
@@ -0,0 +1,29 @@
+import TopFieldButton from 'components/home/buttons/TopFieldButton/TopFieldButton';
+import { useId } from 'react';
+import { ScrollView, View } from 'react-native';
+import { Field } from 'types/apps/group';
+import topFieldButtonGroupStyles from './TopFieldButtonGroup.style';
+
+interface Props {
+  field: Field[];
+  handleFieldPress: (value: string) => void;
+}
+
+const TopFieldButtonGroup = ({ field, handleFieldPress }: Props) => {
+  const fieldKey = useId();
+  return (
+    <View style={topFieldButtonGroupStyles.fieldContainer}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {field.map((fieldElement, _id) => (
+          <TopFieldButton
+            key={fieldKey + _id}
+            field={fieldElement}
+            handlePress={handleFieldPress}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default TopFieldButtonGroup;
