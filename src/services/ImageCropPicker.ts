d14e420033dbb00622966b88416ce88281f09587
Yeonju Jo <yjj0287@gmail.com>
2023-07-29 03:40:04 +0900

    ✨ feat: custom image picker, cropper 추가

---
+++ src/services/ImageCropPicker.ts
@@ -0,0 +1,52 @@
+import ImagePicker, { Image } from 'react-native-image-crop-picker';
+
+const cropperOptions = {
+  cropping: true,
+  cropperStatusBarColor: 'black',
+  cropperToolbarColor: 'black',
+  cropperToolbarWidgetColor: 'white',
+  cropperCancelText: '취소',
+  cropperChooseText: '선택',
+};
+
+export const openImagePicker = async (maxNumber: number): Promise<Image[]> => {
+  try {
+    const images = await ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      multiple: true,
+      maxFiles: maxNumber,
+      mediaType: 'photo',
+    });
+
+    return images;
+  } catch (err) {
+    // TODO: Handle error
+    console.error(err);
+    throw err;
+  }
+};
+
+export const openImageCropper = async (
+  imagePath: string,
+  width: number,
+  height: number,
+): Promise<Image> => {
+  try {
+    const croppedImage: Image = await ImagePicker.openCropper({
+      path: imagePath,
+      width,
+      height,
+      freeStyleCropEnabled: true,
+      includeBase64: true,
+      mediaType: 'photo',
+      ...cropperOptions,
+    });
+
+    return croppedImage;
+  } catch (err) {
+    // TODO: Handle error
+    console.error(err);
+    throw err;
+  }
+};
