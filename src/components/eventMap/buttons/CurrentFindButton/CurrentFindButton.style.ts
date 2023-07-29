2f68874d477bd12cdfe71330e5a9389173a8626f
HoCG <rhwkd5012@naver.com>
2023-07-29 22:37:49 +0900

    💅 design: 현위치 검색버튼 간격개선

--- src/components/eventMap/buttons/CurrentFindButton/CurrentFindButton.style.ts
+++ src/components/eventMap/buttons/CurrentFindButton/CurrentFindButton.style.ts
@@ -5,6 +5,7 @@ const currentFindButtonStyles = StyleSheet.create({
   container: {
     position: 'absolute',
     zIndex: 3,
+    gap: 15,
     top: 50,
     borderWidth: 1,
     justifyContent: 'center',
@@ -12,8 +13,8 @@ const currentFindButtonStyles = StyleSheet.create({
     flexDirection: 'row',
     backgroundColor: 'white',
     borderColor: colors.main,
-    left: Dimensions.get('window').width / 2 - 100,
-    width: 200,
+    left: Dimensions.get('window').width / 2 - 87.5,
+    width: 175,
     borderRadius: 17,
     height: 33,
   },
