252435762e1e4bf591f90d530e0e9b3b15d39229
Yeonju Jo <yjj0287@gmail.com>
2023-07-30 04:03:16 +0900

    🚧 wip: 이벤트 개설 요청 작업중

--- src/screens/home/OpenEventScreen/OpenEventScreen.tsx
+++ src/screens/home/OpenEventScreen/OpenEventScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { ScrollView, View } from 'react-native';
+import { Alert, ScrollView, View } from 'react-native';
 import { useOpenEventStore } from 'stores/OpenEventStore';
 import MENT_OPEN_EVENT from 'constants/openEvent';
 import Spacing from 'components/common/Spacing/Spacing';
@@ -7,20 +7,58 @@ import Divider from 'components/common/Divider/Divider';
 import FixedButton from 'components/common/FixedButton/FixedButton';
 import { OpenEvent, OpenEventForm } from 'components/openEvent';
 import useOpenEventValidator from 'hooks/openEvent/useOpenEventValidator';
+import { useCreateEvent } from 'hooks/queries/event';
+import { CreateNewEventRequestDto } from 'models/event/request/CreateNewEventRequestDto';
+import useNavigator from 'hooks/navigator/useNavigator';
 import openEventScreenStyles from './OpenEventScreen.style';
 
 const OpenEventScreen = () => {
-  const { init, openEvent, setOpenEventErrorMessage } = useOpenEventStore();
+  const { stackNavigation } = useNavigator();
 
+  const { init, openEvent, setOpenEventErrorMessage } = useOpenEventStore();
   const { hasError, errorMessage } = useOpenEventValidator({ openEvent });
+
+  const handleSuccess = () => {
+    // TODO
+    Alert.alert('성공');
+    stackNavigation.goBack();
+  };
+
+  const handleError = () => {
+    // TODO
+  };
+
+  const { mutateAsync: createEvent, isLoading } = useCreateEvent(
+    handleSuccess,
+    handleError,
+  );
 
   const handlePress = () => {
     if (hasError) {
       setOpenEventErrorMessage(errorMessage);
       return;
     }
-    // TODO: submit -> success/fail
+
     console.log(openEvent);
+    const submitForm: CreateNewEventRequestDto = {
+      fieldTypeList: [],
+      title: openEvent.title!,
+      applicationStartDate: openEvent.applicationStartDate!,
+      applicationEndDate: openEvent.applicationEndDate!,
+      eventDates: openEvent.eventDates,
+      streetLoadAddress: openEvent.address.roadAddress!,
+      detailAddress: openEvent.address.detailAddress!,
+      eventFee: openEvent.cost!,
+      maxParticipant: openEvent.recruitmentNumber!,
+      description: openEvent.description!,
+      imageDataList: [],
+      extraQuestionList: openEvent.additionalInformation,
+      hostName: openEvent.hostName!,
+      hostPhoneNumber: openEvent.hostPhoneNumber!,
+      hostEmail: openEvent.hostEmail!,
+    };
+
+    createEvent(submitForm);
   };
 
   useEffect(() => {
@@ -33,13 +71,13 @@ const OpenEventScreen = () => {
         style={openEventScreenStyles.container}
         contentContainerStyle={openEventScreenStyles.containerContent}
       >
+        {/* TODO: 로딩화면제작 */}
         <OpenEvent.Header title={MENT_OPEN_EVENT.INFO} />
 
         <OpenEventForm.Field />
         <OpenEventForm.Title />
         <OpenEventForm.ApplicationPeriod />
         <OpenEventForm.Period />
-        {/* TODO */}
         <OpenEventForm.Address />
         <OpenEventForm.Cost />
         <OpenEventForm.RecruitmentNumber />
@@ -50,10 +88,11 @@ const OpenEventScreen = () => {
         <Divider height={1} color="darkGrey" />
 
         <OpenEvent.Header title={MENT_OPEN_EVENT.INFO} />
-        {/* TODO */}
+
         <OpenEventForm.HostName />
         <OpenEventForm.HostPhoneNumber />
         <OpenEventForm.HostEmail />
+        {/* TODO: 주최자 추가 기능 */}
 
         <Divider height={1} color="darkGrey" />
 
