11aa817088da55ada9a0e17b887d51009d2712a1
Yeonju Jo <yjj0287@gmail.com>
2023-08-01 06:27:13 +0900

    💅 design: event detail screen layout 추가

---
+++ src/components/eventDetail/layout/ScreenLayout.tsx
@@ -0,0 +1,22 @@
+import { CONSTANT_EVENT_DETAIL } from 'constants/eventDetail/eventDetailContants';
+import { PropsWithChildren } from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import screenLayoutStyles from './ScreenLayout.style';
+
+const ScreenLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      style={[
+        screenLayoutStyles.container,
+        {
+          paddingHorizontal: CONSTANT_EVENT_DETAIL.SCREEN_PADDING,
+        },
+      ]}
+    >
+      {children}
+    </KeyboardAvoidingView>
+  );
+};
+
+export default ScreenLayout;
