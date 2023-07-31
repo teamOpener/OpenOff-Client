4c634c069e13327efa2f81e41853bf3a96d2993d
Yeonju Jo <yjj0287@gmail.com>
2023-08-01 06:12:58 +0900

    🚧 wip: 임의로 event detail 페이지 접근 포인트 지정

--- src/components/home/lists/EventCardList/EventCardList.tsx
+++ src/components/home/lists/EventCardList/EventCardList.tsx
@@ -2,6 +2,7 @@ import Text from 'components/common/Text/Text';
 import EventCard from 'components/home/cards/EventCard/EventCard';
 import { ScrollView, View } from 'react-native';
 import { Event } from 'types/event';
+import useNavigator from 'hooks/navigator/useNavigator';
 import eventCardListStyles from './EventCardList.style';
 
 interface Props {
@@ -11,6 +12,12 @@ interface Props {
 }
 
 const EventCardList = ({ events, title, subTitle }: Props) => {
+  // FIXME: 임의로 넣어놨습니다!! - 제인
+  const { stackNavigation } = useNavigator();
+  const handlePress = () => {
+    stackNavigation.navigate('EventDetail', { id: 1 });
+  };
+
   return (
     <View style={eventCardListStyles.container}>
       <Text variant="h3" color="white">
@@ -25,13 +32,7 @@ const EventCardList = ({ events, title, subTitle }: Props) => {
         showsHorizontalScrollIndicator={false}
       >
         {events.map((event) => (
-          <EventCard
-            key={event.id}
-            event={event}
-            handlePress={() => {
-              return false;
-            }}
-          />
+          <EventCard key={event.id} event={event} handlePress={handlePress} />
         ))}
       </ScrollView>
     </View>
