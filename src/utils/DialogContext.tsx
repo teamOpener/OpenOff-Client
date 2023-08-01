9eac86c0b523112ffc4d5781e5c2472178cab518
HoCG <rhwkd5012@naver.com>
2023-08-01 21:34:05 +0900

    ✨ feat: dialogContext 함수인자 개선

--- src/utils/DialogContext.tsx
+++ src/utils/DialogContext.tsx
@@ -1,12 +1,11 @@
 import { createContext } from 'react';
 
 const DialogContext = createContext({
-  openDialog: (text: string, type: string) => {
-    console.log(text);
-  },
-  closeDialog: (callback?: () => void) => {
+  openDialog: (text: string, type: string, callback?: () => void) => {
     if (!callback) return;
     callback();
   },
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  closeDialog: () => {},
 });
 export default DialogContext;
