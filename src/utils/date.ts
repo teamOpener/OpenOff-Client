3fce3d43e3352aa10d544a6d1b101f8957f94cd7
Yeonju Jo <yjj0287@gmail.com>
2023-08-03 10:52:46 +0900

    ✨ feat: date, address util 함수 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 const calcDay = (dayValue: number) => {
   if (dayValue >= 10) {
     return dayValue;
@@ -34,4 +36,38 @@ const serverDateFormatter = (date: Date): string => {
   return `${year}-${month}-${day}T${hours}:${minutes}:00`;
 };
 
-export { calcDay, dateFormatter, formatDateTime, serverDateFormatter };
+const ticketListDateFormatter = (dates: string[]): string => {
+  const sortedDates = dates.sort((a, b) =>
+    dayjs(a).isAfter(dayjs(b)) ? 1 : -1,
+  );
+  const earliestStartDate = dayjs(sortedDates[0]);
+  const latestEndDate = dayjs(sortedDates[sortedDates.length - 1]);
+
+  const formattedStartDate = earliestStartDate.format('MM.DD(ddd)');
+
+  const isMultipleDays = latestEndDate.diff(earliestStartDate, 'day') >= 1;
+
+  let output = '';
+
+  if (isMultipleDays) {
+    output = `${formattedStartDate} ~ ${latestEndDate.format('MM.DD(ddd)')}`;
+  } else {
+    const formattedStartTime = earliestStartDate.format('HH:mm');
+    const formattedEndTime = latestEndDate.format('HH:mm');
+    if (formattedStartTime !== formattedEndTime) {
+      output = `${formattedStartDate} ${formattedStartTime} ~ ${formattedEndTime}`;
+    } else {
+      output = `${formattedStartDate} ${formattedStartTime}`;
+    }
+  }
+
+  return output;
+};
+
+export {
+  calcDay,
+  dateFormatter,
+  formatDateTime,
+  serverDateFormatter,
+  ticketListDateFormatter,
+};
