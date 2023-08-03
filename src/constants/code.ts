708e3a30a3e1a135282782e17f5ef282a1c64168
Yeonju Jo <yjj0287@gmail.com>
2023-08-03 10:46:37 +0900

    ✨ feat: 참여 이벤트 페이지에 필요한 상수, 타입들 추가

--- src/constants/code.ts
+++ src/constants/code.ts
@@ -15,6 +15,34 @@ const enum FieldCode {
   FSDH = 'FSDH',
 }
 
+const enum FieldName {
+  EE = '전시/플리마켓',
+  S = '공연',
+  EA = '운동/액티비티',
+  FD = '푸드/드링크',
+  PF = '파티/페스티벌',
+  FSDH = '친목/일일호프',
+}
+
+const getFieldName = (fieldCode: FieldCode): FieldName => {
+  switch (fieldCode) {
+    case FieldCode.EE:
+      return FieldName.EE;
+    case FieldCode.S:
+      return FieldName.S;
+    case FieldCode.EA:
+      return FieldName.EA;
+    case FieldCode.FD:
+      return FieldName.FD;
+    case FieldCode.PF:
+      return FieldName.PF;
+    case FieldCode.FSDH:
+      return FieldName.FSDH;
+    default:
+      throw new Error(`Invalid FieldCode: ${fieldCode}`);
+  }
+};
+
 /** 
  *- CA: Comment Alert
   - EA: Event Alert
@@ -24,4 +52,4 @@ const enum AlertCode {
   EA = 'EA',
 }
 
-export { AlertCode, FieldCode };
+export { AlertCode, FieldCode, FieldName, getFieldName };
