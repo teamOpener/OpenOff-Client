ddc715440c0096f15e17a81836486a1a48d73f0e
HoCG <rhwkd5012@naver.com>
2023-08-04 22:02:07 +0900

    ✨ feat: user전용 메뉴버튼들 구현

---
+++ src/components/user/groups/UserMenuButtonGroup/UserMenuButtonGroup.style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const userMenuButtonGroupStyles = StyleSheet.create({
+  container: {
+    gap: 40,
+    flexDirection: 'row',
+  },
+  menuButton: {
+    gap: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default userMenuButtonGroupStyles;
