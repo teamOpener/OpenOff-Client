92d3ef8afee28d6197915bbd1c6449dc933cce4d
Yeonju Jo <yjj0287@gmail.com>
2023-08-05 04:55:38 +0900

    🚧 wip: 주최자 페이지 params 불러오도록 추가

--- src/screens/userEvent/HostLedgerScreen/HostLedgerScreen.tsx
+++ src/screens/userEvent/HostLedgerScreen/HostLedgerScreen.tsx
@@ -1,7 +1,11 @@
-import Text from 'components/common/Text/Text';
 import { View } from 'react-native';
+import Text from 'components/common/Text/Text';
+import { StackMenu } from 'constants/menu';
+import useRouteParams from 'hooks/navigator/useRouteParams';
 
 const HostLedgerScreen = () => {
+  const params = useRouteParams<StackMenu.HostLedger>();
+
   return (
     <View>
       <Text>명단관리</Text>
