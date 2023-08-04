ebd3af3314ea000ff0c993aea3ed3dadaa5985d2
HoCG <rhwkd5012@naver.com>
2023-08-04 22:02:49 +0900

    ✨ feat: user mock data추가

---
+++ src/mocks/user/userInfo.ts
@@ -0,0 +1,12 @@
+const userInfo = {
+  nickname: '죠스타',
+  profileImage: null,
+  email: 'jojo123@naver.com',
+  interestField: ['EA', 'FD'],
+  name: '황호세',
+  password: '*******',
+  phoneNumber: '01036452322',
+  birth: '1997-05-12',
+};
+
+export default userInfo;
