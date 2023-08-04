fb721ca5a5ff0fe821b93f664fabd2002f840f9e
HoCG <rhwkd5012@naver.com>
2023-08-04 22:06:36 +0900

    ✨ feat: 이벤트 카드를 scrap 스크린에서 사용할 수 있도록 재구성

--- src/components/home/cards/EventCard/EventCard.tsx
+++ src/components/home/cards/EventCard/EventCard.tsx
@@ -1,24 +1,37 @@
 import Icon from 'components/common/Icon/Icon';
 import Text from 'components/common/Text/Text';
-import { Image, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Image, TouchableOpacity, View } from 'react-native';
 import { Event } from 'types/event';
 import eventCardStyles from './EventCard.style';
 
 interface Props {
   event: Event;
+  type?: 'default' | 'scrap';
   handlePress: () => void;
 }
 
-const EventCard = ({ event, handlePress }: Props) => {
+const EventCard = ({ event, type = 'default', handlePress }: Props) => {
+  const calcWidth =
+    type === 'default' ? 200 : Dimensions.get('window').width / 2 - 30;
   return (
-    <View style={eventCardStyles.container}>
+    <View
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        ...eventCardStyles.container,
+        marginBottom: type === 'default' ? 0 : 20,
+      }}
+    >
       <TouchableOpacity onPress={handlePress}>
         <Image
-          style={eventCardStyles.image}
+          style={{
+            ...eventCardStyles.image,
+            width: calcWidth,
+            height: calcWidth,
+          }}
           source={{ uri: event.images[0] }}
         />
       </TouchableOpacity>
-      <Text variant="body2" color="white">
+      <Text variant="body2" color={type === 'default' ? 'white' : 'main'}>
         {event.name}
       </Text>
       <View style={eventCardStyles.iconText}>
@@ -34,11 +47,12 @@ const EventCard = ({ event, handlePress }: Props) => {
         </Text>
       </View>
       <TouchableOpacity style={eventCardStyles.likeButton}>
-        {event.like ? (
-          <Icon name="IconFillHeart" size={20} fill="main" />
-        ) : (
-          <Icon name="IconHeart" size={20} />
-        )}
+        {type === 'default' &&
+          (event.like ? (
+            <Icon name="IconFillHeart" size={20} fill="main" />
+          ) : (
+            <Icon name="IconHeart" size={20} />
+          ))}
       </TouchableOpacity>
     </View>
   );
