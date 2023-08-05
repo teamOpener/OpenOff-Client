4e29d893c93cfd2239df25486a5da562dc6bb52c
HoCG <rhwkd5012@naver.com>
2023-08-05 16:21:00 +0900

    ✨ feat: 사용자 댓글 카드 구현

--- src/components/user/cards/UserCommentCard/UserCommentCard.tsx
+++ src/components/user/cards/UserCommentCard/UserCommentCard.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Pressable } from 'react-native';
 import Text from 'components/common/Text/Text';
 import UserComment from 'types/apps/comment';
 import dayjs from 'dayjs';
+import Icon from 'components/common/Icon/Icon';
+import userCommentCardStyles from './UserCommentCard.style';
 
 interface Props {
   comment: UserComment;
+  handleArrowPress: () => void;
 }
 
-const UserCommentCard = ({ comment }: Props) => {
+const UserCommentCard = ({ comment, handleArrowPress }: Props) => {
   return (
-    <View>
-      <Image source={{ uri: comment.eventImage }} />
-      <View>
-        <Text variant="bodySB" color="main">
-          {comment.eventName}
-        </Text>
-        <Text variant="body2" color="white">
-          {comment.comment}
-        </Text>
-        <Text variant="body3" color="white">
-          {dayjs(comment.regDate).format('YYYY.MM.DD')}
-        </Text>
+    <View style={userCommentCardStyles.container}>
+      <View style={userCommentCardStyles.baseContainer}>
+        <Image
+          style={userCommentCardStyles.image}
+          source={{ uri: comment.eventImage }}
+        />
+        <View style={userCommentCardStyles.commentInfo}>
+          <Text variant="bodySB" color="main">
+            {comment.eventName}
+          </Text>
+          <Text variant="body2" color="white">
+            {comment.comment}
+          </Text>
+          <Text variant="body3" color="white">
+            {dayjs(comment.regDate).format('YYYY.MM.DD')}
+          </Text>
+        </View>
       </View>
+      <Pressable onPress={handleArrowPress}>
+        <Icon size={10} name="IconArrowRight" />
+      </Pressable>
     </View>
   );
 };
