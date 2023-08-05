56fc3bca8db82456ff237a11eafed1d14d806806
HoCG <rhwkd5012@naver.com>
2023-08-05 16:22:56 +0900

    ✨ feat: 사용자 댓글 화면 구현

---
+++ src/screens/user/UserCommentScreen/UserCommentScreen.style.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const userCommentScreenStyles = StyleSheet.create({
+  container: {
+    padding: 30,
+  },
+});
+
+export default userCommentScreenStyles;
--- src/screens/user/UserCommentScreen/UserCommentScreen.tsx
+++ src/screens/user/UserCommentScreen/UserCommentScreen.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import { View } from 'react-native';
+import userComment from 'mocks/user/userComment';
+import UserCommentCard from 'components/user/cards/UserCommentCard/UserCommentCard';
+import userCommentScreenStyles from './UserCommentScreen.style';
 
 const UserCommentScreen = () => {
-  return <View />;
+  const handleArrowPress = () => {
+    return false;
+  };
+  return (
+    <View style={userCommentScreenStyles.container}>
+      {userComment.map((comment) => (
+        <UserCommentCard
+          key={comment.id}
+          handleArrowPress={handleArrowPress}
+          comment={comment}
+        />
+      ))}
+    </View>
+  );
 };
 
 export default UserCommentScreen;
