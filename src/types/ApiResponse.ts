db1b1b6262ee63e0988871533715d2c885a2e4ae
Yeonju Jo <yjj0287@gmail.com>
2023-08-08 07:43:14 +0900

    ✨ feat: api response type 정의

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,8 +1,12 @@
-type ApiResponse<T> = {
+import { AxiosError } from 'axios';
+
+export type ApiResponse<T = object> = {
   code: number;
   message: string;
   timestamp: Date;
   data: T | undefined;
 };
+
+export type ApiErrorResponse = AxiosError<ApiResponse>;
 
 export default ApiResponse;
