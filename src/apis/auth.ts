2e3f4736feb18d66261d83f29e9ecd02387eacf2
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-08-08 18:24:21 +0900

    ✨ feat: 구현에 필요한 API 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,12 +1,56 @@
 import fetcher from 'apis';
-import { ApiResponse } from 'types/ApiResponse';
 import { NormalSignInRequestDto } from 'models/auth/request/NormalSignInRequestDto';
+import { SocialSignupRequestDto } from 'models/auth/request/SocialSignupRequestDto';
+import { TokenRequestDto } from 'models/auth/request/TokenRequestDto';
+import { CheckEmailResponseDto } from 'models/auth/response/CheckEmailResponseDto';
 import { TokenResponseDto } from 'models/auth/response/TokenResponseDto';
+import { useAuthorizeStore } from 'stores/Authorize';
+import { ApiResponse } from 'types/ApiResponse';
 
-// eslint-disable-next-line import/prefer-default-export
+const { token } = useAuthorizeStore.getState();
+
 export const normalLogin = async (
   data: NormalSignInRequestDto,
 ): Promise<ApiResponse<TokenResponseDto>> => {
   const response = await fetcher.post(`auth/login/normal`, data);
+  return response.data;
+};
+
+export const normalSignUp = async (
+  data: NormalSignInRequestDto,
+): Promise<ApiResponse<TokenResponseDto>> => {
+  const response = await fetcher.post(`/auth/signup/normal`, data);
+  return response.data;
+};
+
+export const refresh = async (): Promise<ApiResponse<TokenResponseDto>> => {
+  const params: TokenRequestDto = {
+    accessToken: token.accessToken,
+    refreshToken: token.refreshToken,
+  };
+  const response = await fetcher.post(`auth/refresh`, params);
+  return response.data;
+};
+
+export const socialLogin = async (
+  data: SocialSignupRequestDto,
+): Promise<ApiResponse<TokenResponseDto>> => {
+  const params = {
+    token: data.token,
+  };
+  const response = await fetcher.post(
+    `/auth/signup/social/${data.socialType}`,
+    params,
+  );
+  return response.data;
+};
+
+export const checkEmail = async (
+  email: string,
+): Promise<ApiResponse<CheckEmailResponseDto>> => {
+  const params = {
+    email,
+  };
+  const response = await fetcher.get(`/auth/check/email`, { params });
   return response.data;
 };
---
+++ src/apis/field.ts
@@ -0,0 +1,11 @@
+import fetcher from 'apis';
+import AddInterestRequestDto from 'models/field/request/AddInterestRequestDto';
+import { ApiResponse } from 'types/ApiResponse';
+
+// eslint-disable-next-line import/prefer-default-export
+export const updateInterestField = async (
+  data: AddInterestRequestDto,
+): Promise<ApiResponse> => {
+  const response = await fetcher.post(`field/save`, data);
+  return response.data;
+};
