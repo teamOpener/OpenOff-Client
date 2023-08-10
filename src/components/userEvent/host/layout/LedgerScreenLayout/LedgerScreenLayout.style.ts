06896bcd01e7895eca5669d897f354f992068de4
Yeonju Jo <yjj0287@gmail.com>
2023-08-11 02:01:05 +0900

    ✨ feat: 명단 페이지 레이아웃 추가

---
+++ src/components/userEvent/host/layout/LedgerScreenLayout/LedgerScreenLayout.style.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { layouts } from 'styles/theme';
+
+const ledgerScreenLayoutStyles = StyleSheet.create({
+  container: {
+    paddingHorizontal: layouts.PADDING,
+    paddingTop: layouts.PADDING,
+  },
+});
+
+export default ledgerScreenLayoutStyles;
