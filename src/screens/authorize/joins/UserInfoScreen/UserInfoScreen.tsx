7aed2316555162489482e765962ee3702b77324c
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-08-10 14:46:41 +0900

    ✨ feat: nickname DTO추가 및 userInfoScreen 개선

--- src/screens/authorize/joins/UserInfoScreen/UserInfoScreen.tsx
+++ src/screens/authorize/joins/UserInfoScreen/UserInfoScreen.tsx
@@ -17,30 +17,34 @@ interface Props {
 
 const UserInfoScreen = ({ dispatch }: Props) => {
   const navigation = useNavigation<NavigationProp<AuthStackParamList>>();
-  const [name, setName] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
   const [birth, setBirth] = useState<string>('2000-00-00');
   const [gender, setGender] = useState<Gender>('MAN');
+
   const isActive =
-    !validateName(name) && !validateBirthday(birth) && name.length > 1;
+    !validateName(username) && !validateBirthday(birth) && username.length > 1;
+
+  const handleAuthorizeFlow = () => {
+    dispatch({ type: UserInfoStatus.SET_NAME, username });
+    dispatch({ type: UserInfoStatus.SET_GENDER, gender });
+    dispatch({ type: UserInfoStatus.SET_BIRTH, birth });
+    navigation.navigate(AuthorizeMenu.InterestField);
+  };
+
   return (
     <ScreenCover
       titleElements={['오픈오프 이용을 위해', '정보를 입력해주세요.']}
       authorizeButton={{
-        handlePress: () => {
-          dispatch({ type: UserInfoStatus.SET_NAME, name });
-          dispatch({ type: UserInfoStatus.SET_GENDER, gender });
-          dispatch({ type: UserInfoStatus.SET_BIRTH, birth });
-          navigation.navigate(AuthorizeMenu.InterestField);
-        },
+        handlePress: handleAuthorizeFlow,
         label: '확인',
         isActive,
       }}
     >
       <BaseInfoInput
         label="이름"
-        value={name}
+        value={username}
         width={Dimensions.get('window').width - 50}
-        setValue={setName}
+        setValue={setUsername}
         validation={validateName}
       />
       <View style={userInfoScreenStyles.detailUserInfo}>
