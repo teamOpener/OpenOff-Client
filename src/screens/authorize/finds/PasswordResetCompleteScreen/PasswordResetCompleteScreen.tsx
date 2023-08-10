eb4011b4934dc1ca3d4071ba71fd9f75f48b295b
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-08-10 17:05:21 +0900

    ✨ feat: 이메일 찾기 \& 비밀번호 완료화면에 로그인 돌아가기 버튼 추가

--- src/screens/authorize/finds/PasswordResetCompleteScreen/PasswordResetCompleteScreen.tsx
+++ src/screens/authorize/finds/PasswordResetCompleteScreen/PasswordResetCompleteScreen.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import ScreenCover from 'components/authorize/covers/ScreenCover/ScreenCover';
 import Text from 'components/common/Text/Text';
+import { AuthorizeMenu } from 'constants/menu';
+import { Image, View } from 'react-native';
+import { AuthStackParamList } from 'types/apps/menu';
 import passwordResetCompleteScreenStyles from './PasswordResetCompleteScreen.style';
 
 const PasswordResetCompleteScreen = () => {
+  const navigation = useNavigation<NavigationProp<AuthStackParamList>>();
   return (
-    <View style={passwordResetCompleteScreenStyles.authorizeContainer}>
-      <Image
-        style={passwordResetCompleteScreenStyles.checkImage}
-        source={require('../../../../assets/images/check.png')}
-      />
-      <Text variant="h4" color="main">
-        비밀번호 변경이 완료되었습니다.
-      </Text>
-      <Text variant="body2" color="white">
-        다시 로그인을 해주세요.
-      </Text>
-    </View>
+    <ScreenCover
+      authorizeButton={{
+        handlePress: () => navigation.navigate(AuthorizeMenu.Login),
+        label: '로그인 화면으로',
+        isActive: true,
+      }}
+    >
+      <View style={passwordResetCompleteScreenStyles.authorizeContainer}>
+        <Image
+          style={passwordResetCompleteScreenStyles.checkImage}
+          source={require('../../../../assets/images/check.png')}
+        />
+        <Text variant="h4" color="main">
+          비밀번호 변경이 완료되었습니다.
+        </Text>
+        <Text variant="body2" color="white">
+          다시 로그인을 해주세요.
+        </Text>
+      </View>
+    </ScreenCover>
   );
 };
 
