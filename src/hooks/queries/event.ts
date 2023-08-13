3f9d448bcbc386bb7f75fa1ad4eafbcd6d1ffbff
HoCG <rhwkd5012@naver.com>
2023-08-13 23:09:12 +0900

    ✨ feat: 각 이벤트 화면을 위한 쿼리 추가

--- src/hooks/queries/event.ts
+++ src/hooks/queries/event.ts
@@ -1,8 +1,14 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query';
 import queryKeys from 'constants/queryKeys';
 import fakeApi from 'apis/test';
 import { CreateNewEventRequestDto } from 'models/event/request/CreateNewEventRequestDto';
-import { getEventDetailInfo } from 'apis/eventInstance';
+import {
+  getEventDetailInfo,
+  getFieldEventLists,
+  getPersonalEventLists,
+  getVogueEventLists,
+} from 'apis/eventInstance';
+import { FieldCode } from 'constants/code';
 
 // TODO: 생성
 export const useCreateEvent = (
@@ -35,4 +41,69 @@ export const useApplyEvent = (
     onSuccess: successCallback,
     onError: errorCallback,
   });
+};
+
+export const useFieldEventLists = (field: FieldCode) => {
+  const query = useInfiniteQuery(
+    [...queryKeys.eventKeys.listByField(field)],
+    ({ pageParam = undefined }) =>
+      getFieldEventLists({
+        eventInfoId: pageParam,
+        field,
+      }),
+    {
+      getNextPageParam: (lastPage) => {
+        const lastIdx = lastPage.data?.content.length;
+        if (!lastPage.data?.hasNext) {
+          return false;
+        }
+        return lastPage.data?.content[lastIdx - 1].eventInfoId;
+      },
+      suspense: false,
+    },
+  );
+  return query;
+};
+
+export const useVogueEventInfiniteLists = () => {
+  return useInfiniteQuery(
+    [...queryKeys.eventKeys.vogueInfiniteList],
+    ({ pageParam = { count: undefined, eventInfoId: undefined } }) =>
+      getVogueEventLists({
+        count: pageParam.count,
+        eventInfoId: pageParam.eventInfoId,
+      }),
+    {
+      getNextPageParam: (lastPage) => {
+        const lastIdx = lastPage.data?.content.length;
+        if (!lastPage.data?.hasNext) {
+          return false;
+        }
+        return {
+          eventInfoId: lastPage.data?.content[lastIdx - 1].eventInfoId,
+          count: lastPage.data?.content[lastIdx - 1].totalApplicantCount,
+        };
+      },
+    },
+  );
+};
+
+export const useVogueEventLists = () => {
+  return useQuery(
+    [...queryKeys.eventKeys.vogueList],
+    () => getVogueEventLists(),
+    {
+      select: (data) => data.data,
+    },
+  );
+};
+
+export const usePersonalEventLists = () => {
+  return useQuery(
+    [...queryKeys.eventKeys.personalList],
+    () => getPersonalEventLists(),
+    {
+      select: (data) => data.data,
+    },
+  );
 };
