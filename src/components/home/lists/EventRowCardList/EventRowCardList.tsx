21fa504b77f03c45eeefa12ae601316f954ebc6d
HoCG <rhwkd5012@naver.com>
2023-08-13 23:03:36 +0900

    ✨ feat: eventRowCard에 이벤트 데이터 바인딩

--- src/components/home/lists/EventRowCardList/EventRowCardList.tsx
+++ src/components/home/lists/EventRowCardList/EventRowCardList.tsx
@@ -1,24 +1,49 @@
+import { InfiniteData } from '@tanstack/react-query';
 import EventRowCard from 'components/home/cards/EventRowCard/EventRowCard';
+import EventRowCardSkeleton from 'components/suspense/skeleton/EventRowCardSkeleton/EventRowCardSkeleton';
+import { useEffect } from 'react';
 import { FlatList } from 'react-native';
+import { InfiniteScrollApiResponse } from 'types/ApiResponse';
 import { Event } from 'types/event';
 
 interface Props {
-  eventList: Event[];
+  pageData?: InfiniteData<InfiniteScrollApiResponse<Event>>;
+  isFetching: boolean;
+  isLoading: boolean;
+  hasNextPage?: boolean;
+  handleEndReached: () => void;
 }
 
-const EventRowCardList = ({ eventList }: Props) => {
+const EventRowCardList = ({
+  pageData,
+  isFetching,
+  isLoading,
+  hasNextPage,
+  handleEndReached,
+}: Props) => {
+  const flatEventRowList = pageData?.pages.flatMap((page) => page.data.content);
+
+  const isHasNextSkeleton =
+    hasNextPage && flatEventRowList?.length !== 0 && isFetching;
+
   return (
     <FlatList
       numColumns={1}
-      data={eventList}
+      data={flatEventRowList}
       renderItem={(event) => (
-        <EventRowCard key={event.item.eventInfoId} event={event.item} />
+        <EventRowCard key={`eventCard-${event.index}`} event={event.item} />
       )}
-      // ListFooterComponent={isFetching ? <EventRowCardSkeleton /> : undefined}
+      ListFooterComponent={
+        isHasNextSkeleton || isLoading ? (
+          <>
+            {new Array(6).fill(1).map((_, _idx) => (
+              <EventRowCardSkeleton key={_idx} />
+            ))}
+          </>
+        ) : undefined
+      }
       onEndReachedThreshold={0.5}
-      onEndReached={() => {
-        return false;
-      }}
+      onEndReached={handleEndReached}
     />
   );
 };
