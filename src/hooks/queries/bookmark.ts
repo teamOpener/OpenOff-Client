5f5738f3bc79a90e144fadc4b6511ab1028168a4
HoCG <rhwkd5012@naver.com>
2023-08-13 23:09:40 +0900

    ✨ feat: 찜한목록 쿼리 구현

---
+++ src/hooks/queries/bookmark.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getBookmarkEventLists } from 'apis/bookmark';
+import queryKeys from 'constants/queryKeys';
+
+export const useBookmarkEventLists = () => {
+  return useInfiniteQuery(
+    [...queryKeys.bookmarkKeys.list],
+    ({ pageParam = undefined }) => getBookmarkEventLists(pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        const lastIdx = lastPage.data?.content.length;
+        if (!lastPage.data?.hasNext) {
+          return false;
+        }
+        return lastPage.data?.content[lastIdx - 1].bookmarkId;
+      },
+    },
+  );
+};
