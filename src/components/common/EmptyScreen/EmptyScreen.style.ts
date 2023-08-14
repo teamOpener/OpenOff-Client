5da0e54321d852a2a972d0053fc382f9fa7871b1
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-08-14 10:39:50 +0900

    ✨ feat: 비어있는 리스트 화면 구현

---
+++ src/components/common/EmptyScreen/EmptyScreen.style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const emptyScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 20,
+  },
+  emptyImage: {
+    width: 100,
+    height: 100,
+  },
+});
+
+export default emptyScreenStyles;
