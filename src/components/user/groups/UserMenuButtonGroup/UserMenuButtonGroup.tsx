73de5bacc31c52979761c38d9211e72b5b293628
HoCG <rhwkd5012@naver.com>
2023-08-15 17:30:06 +0900

    ✨ feat: 사용자 메뉴 버튼 그룹에 네비게이팅 추가

--- src/components/user/groups/UserMenuButtonGroup/UserMenuButtonGroup.tsx
+++ src/components/user/groups/UserMenuButtonGroup/UserMenuButtonGroup.tsx
@@ -2,33 +2,57 @@ import Icon from 'components/common/Icon/Icon';
 import Text from 'components/common/Text/Text';
 import React from 'react';
 import { Pressable, View } from 'react-native';
+import useNavigator from 'hooks/navigator/useNavigator';
 import userMenuButtonGroupStyles from './UserMenuButtonGroup.style';
 
 const UserMenuButtonGroup = () => {
+  const { stackNavigation, tabNavigation } = useNavigator();
+  const MY_TICKET = '내 티켓';
+  const MY_SCRAP = '스크랩';
+  const HOST_EVENT = '주최 이벤트';
+  const MY_COMMENT = '내가 쓴 댓글';
+
+  const handleShowScrap = () => {
+    stackNavigation.navigate('Scrap');
+  };
+
+  const handleShowMyTicket = () => {
+    tabNavigation.navigate('UserEvent');
+  };
+
   return (
     <View style={userMenuButtonGroupStyles.container}>
-      <Pressable style={userMenuButtonGroupStyles.menuButton}>
+      <Pressable
+        style={userMenuButtonGroupStyles.menuButton}
+        onPress={handleShowMyTicket}
+      >
         <Icon name="IconVoidTicket" size={30} fill="white" />
         <Text variant="bodySB" color="white">
-          내 티켓
+          {MY_TICKET}
         </Text>
       </Pressable>
-      <Pressable style={userMenuButtonGroupStyles.menuButton}>
+      <Pressable
+        onPress={handleShowScrap}
+        style={userMenuButtonGroupStyles.menuButton}
+      >
         <Icon name="IconBookmark" size={30} fill="white" />
         <Text variant="bodySB" color="white">
-          스크랩
+          {MY_SCRAP}
         </Text>
       </Pressable>
-      <Pressable style={userMenuButtonGroupStyles.menuButton}>
+      <Pressable
+        style={userMenuButtonGroupStyles.menuButton}
+        onPress={handleShowMyTicket}
+      >
         <Icon name="IconCongrates" size={30} fill="white" />
         <Text variant="bodySB" color="white">
-          주최 이벤트
+          {HOST_EVENT}
         </Text>
       </Pressable>
       <Pressable style={userMenuButtonGroupStyles.menuButton}>
         <Icon name="IconComment" size={30} fill="white" />
         <Text variant="bodySB" color="white">
-          내가 쓴 댓글
+          {MY_COMMENT}
         </Text>
       </Pressable>
     </View>
