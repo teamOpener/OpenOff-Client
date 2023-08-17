19e3bdc7ee5232a510ea087d0710b1feeb79d496
Yeonju Jo <yjj0287@gmail.com>
2023-08-18 02:45:42 +0900

    ✨ feat: event calendar 제작

---
+++ src/components/eventDetail/atoms/EventCalendar/EventCalendar.tsx
@@ -0,0 +1,117 @@
+import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
+import { Calendar } from 'react-native-calendars';
+import { MarkedDates, Theme } from 'react-native-calendars/src/types';
+import Spacing from 'components/common/Spacing/Spacing';
+import { EventDetail } from 'components/eventDetail';
+import SpaceLayout from 'components/layout/Space/SpaceLayout';
+import { EventIndexStatisticsDto } from 'models/event/response/EventIndexStatisticsDto';
+import { colors } from 'styles/theme';
+import useEventIndexList from 'hooks/event/useEventIndexList';
+
+interface Props {
+  selectedDate: string;
+  setSelectedDate: React.Dispatch<string>;
+  selectedIndexId: number | null;
+  setSelectedIndexId: React.Dispatch<number | null>;
+  eventIndexList?: EventIndexStatisticsDto[];
+  maxCapacity: number;
+}
+
+const EventCalendar = ({
+  selectedDate,
+  setSelectedDate,
+  selectedIndexId,
+  setSelectedIndexId,
+  eventIndexList = [],
+  maxCapacity,
+}: Props) => {
+  const theme: Theme = {
+    monthTextColor: colors.background,
+    arrowColor: colors.main,
+    disabledArrowColor: colors.grey,
+    textSectionTitleColor: colors.darkGrey,
+    dayTextColor: colors.grey,
+    todayTextColor: colors.grey,
+    textDisabledColor: colors.grey,
+    selectedDayBackgroundColor: colors.point,
+    selectedDayTextColor: colors.white,
+    textMonthFontSize: 20,
+    textDayHeaderFontSize: 12,
+    textDayFontSize: 16,
+  };
+
+  const { eventDateArray, sortedEventDateArray, filterEventsBySelectedDate } =
+    useEventIndexList({ eventIndexList });
+
+  const filteredArray = filterEventsBySelectedDate(
+    eventIndexList,
+    selectedDate,
+  );
+
+  const [markedDates, setMarkedDates] = useState<MarkedDates | null>(null);
+
+  const handleSelectIndexCard = (idx: number) => {
+    setSelectedIndexId(idx);
+  };
+
+  //   TODO click할 때마다 변경
+  useEffect(() => {
+    const newMarkedDates: MarkedDates = {};
+
+    if (markedDates !== null) {
+      return;
+    }
+
+    eventDateArray.forEach((date) => {
+      const dateString = dayjs(date).format('YYYY-MM-DD');
+      newMarkedDates[dateString] = {
+        selected: selectedDate === dateString,
+        customStyles: {
+          text: {
+            color: selectedDate === dateString ? colors.white : colors.black,
+          },
+        },
+      };
+    });
+
+    setMarkedDates(newMarkedDates);
+  }, [eventDateArray]);
+
+  return (
+    <>
+      <Calendar
+        monthFormat="yyyy.MM"
+        minDate={sortedEventDateArray[0]}
+        maxDate={sortedEventDateArray[sortedEventDateArray.length - 1]}
+        hideExtraDays
+        markingType="custom"
+        markedDates={markedDates ?? undefined}
+        theme={theme}
+        onDayPress={(date) => {
+          // TODO 없는 날짜는 클릭시 막기
+          setSelectedDate(date.dateString);
+        }}
+      />
+
+      <Spacing height={20} />
+
+      <SpaceLayout size={10}>
+        {filteredArray &&
+          filteredArray.map((eventIndex) => (
+            <EventDetail.DateSelectButton
+              key={eventIndex.eventIndexId}
+              // TODO: 신청한 날짜, 마감 날짜 등등 disabled 처리
+              isSelected={eventIndex.eventIndexId === selectedIndexId}
+              eventDate={eventIndex.eventDate}
+              approvedUserCount={eventIndex.approvedUserCount}
+              maxCapacity={maxCapacity}
+              onPress={() => handleSelectIndexCard(eventIndex.eventIndexId)}
+            />
+          ))}
+      </SpaceLayout>
+    </>
+  );
+};
+
+export default EventCalendar;
--- src/components/eventDetail/atoms/index.ts
+++ src/components/eventDetail/atoms/index.ts
@@ -5,6 +5,7 @@ export { default as TextButton } from './buttons/TextButton/TextButton';
 // export { default as CommentInput } from './comments/CommentInput/CommentInput';
 // export { default as DeclarationButton } from './comments/DeclarationButton/DeclarationButton';
 // export { default as ParentCommentList } from './comments/ParentCommentList/ParentCommentList';
+export { default as EventCalendar } from './EventCalendar/EventCalendar';
 export { default as DateCard } from './DateCard/DateCard';
 export { default as DateCardCarousel } from './DateCard/DateCardCarousel';
 export { default as DefaultSimpleList } from './lists/DefaultSimpleList/DefaultSimpleList';
