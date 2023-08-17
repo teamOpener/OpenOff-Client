8af8c474b8c02ebbaf29af0ad63d0dc0f062ab23
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-08-17 15:47:39 +0900

    ✨ feat: event card title 길이값 상수화

--- src/components/home/cards/EventRowCard/EventRowCard.style.ts
+++ src/components/home/cards/EventRowCard/EventRowCard.style.ts
@@ -1,6 +1,8 @@
 import { Dimensions, StyleSheet } from 'react-native';
 import { colors, fonts } from 'styles/theme';
 
+const PAGE_AND_CARD_INTERNAL_LENGTH = 170;
+
 const eventRowCardStyles = StyleSheet.create({
   container: {
     borderRadius: 6,
@@ -36,7 +38,7 @@ const eventRowCardStyles = StyleSheet.create({
     backgroundColor: '#d9d9d9',
   },
   eventTitle: {
-    width: Dimensions.get('window').width - 170,
+    width: Dimensions.get('window').width - PAGE_AND_CARD_INTERNAL_LENGTH,
     fontFamily: fonts.semibold,
     fontSize: 15,
     lineHeight: 21,
