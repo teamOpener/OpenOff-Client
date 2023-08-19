47283b081fbb0ebf94eb3e0d1c35dab016ad1279
Yeonju Jo <yjj0287@gmail.com>
2023-08-20 01:12:20 +0900

    ✨ feat: 주최 페이지 멘트 추가

--- src/constants/userEvent/host/hostMessage.ts
+++ src/constants/userEvent/host/hostMessage.ts
@@ -13,14 +13,18 @@ const MENT_HOST = Object.freeze({
     QR_SCAN_MAIN_INFO: 'QR 티켓을 스캔해주세요.',
     QR_SCAN_SUB_INFO: '네모박스에 맞추어 QR코드를 스캔해주세요.',
     EMPTY_LEDGER: '아직 신청인원이 존재하지 않습니다.',
+    CANCEL_BUTTON: '이벤트 취소',
+    CANCEL_TITLE: '이벤트를 취소하시겠습니까?',
+    CANCEL_DESCRIPTION: `개설된 이벤트가 삭제되며, 신청자들에게 취소 알림이 발송됩니다. \n*잦은 취소는 서비스 정지 사유에 해당됩니다`,
+    SUSPENSE_EVENT: '이벤트 신청을 중단하시겠습니까?',
+    PERMIT_ALL: '일괄 승인하시겠습니까?',
   },
-
   SUCCESS: {
-    //
+    SUSPENSE_EVENT: '이벤트 신청이 중단되었습니다.',
+    PERMIT_ALL: '일괄 승인되었습니다.',
   },
-
   ERROR: {
-    UNAPPROVED: '승인되지 않은 이벤트입니다.\n승인완료 후 다시 시도해주세요.',
+    UNAPPROVED: '승인되지 않은 이벤트입니다. \n승인완료 후 다시 시도해주세요.',
     OVERFLOW_AVAILABLE: '신청 가능 인원보다 많습니다.',
   },
 });
