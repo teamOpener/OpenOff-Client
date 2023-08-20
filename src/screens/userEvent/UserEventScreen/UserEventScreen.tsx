21ab31771a345c73a4c9f6c1f9e3283fb9794e37
Yeonju Jo <yjj0287@gmail.com>
2023-08-21 00:25:13 +0900

    ✨ feat: 승인되지 않은 이벤트는 주최자 콘솔 페이지로 이동 못하도록 함

--- src/screens/userEvent/UserEventScreen/UserEventScreen.tsx
+++ src/screens/userEvent/UserEventScreen/UserEventScreen.tsx
@@ -1,12 +1,13 @@
-import { ScrollView, View, FlatList } from 'react-native';
-import { useEffect, useState } from 'react';
+import { View, FlatList } from 'react-native';
+import { useState } from 'react';
+import { HostEventInfoResponseDto } from 'models/ledger/response/HostEventInfoResponseDto';
 import fieldInitData from 'constants/userEvent/participant/fieldData';
 import { UserEventTabItem } from 'constants/userEvent/participant/participantConstants';
 import MENT_PARTICIPANT from 'constants/userEvent/participant/participantMessage';
 import { useHostEventLists, useUserTicketLists } from 'hooks/queries/ledger';
 import useNavigator from 'hooks/navigator/useNavigator';
+import useDialog from 'hooks/app/useDialog';
 import { FieldDataType } from 'types/event/filedDataType';
-import SpaceLayout from 'components/layout/Space/SpaceLayout';
 import Spacing from 'components/common/Spacing/Spacing';
 import Text from 'components/common/Text/Text';
 import {
@@ -18,11 +19,11 @@ import {
 import MENT_HOST from 'constants/userEvent/host/hostMessage';
 import userEventScreenStyles from './UserEventScreen.style';
 
+// TODO skeleton
+
 const UserEventScreen = () => {
   const { stackNavigation } = useNavigator();
-
-  // TODO: 무한스크롤
-  const { data: ticketLists, isLoading } = useUserTicketLists();
+  const { openDialog } = useDialog();
 
   const [activeTabName, setActiveTabName] = useState<UserEventTabItem>(
     UserEventTabItem.PARTICIPANT,
@@ -31,11 +32,24 @@ const UserEventScreen = () => {
   const [field, setField] = useState<FieldDataType[]>(fieldInitData);
   const activeField = field.find((fieldData) => fieldData.isActive);
 
+  // TODO: 무한스크롤 test 필요
+  const { data: ticketLists } = useUserTicketLists(
+    activeTabName === UserEventTabItem.PARTICIPANT
+      ? activeField?.value
+      : undefined,
+  );
+
+  const flatUserTicketList = ticketLists?.pages.flatMap(
+    (page) => page.data.content,
+  );
+
   const {
     data: hostEventList,
     hasNextPage,
     fetchNextPage,
-  } = useHostEventLists(activeField?.value);
+  } = useHostEventLists(
+    activeTabName === UserEventTabItem.HOST ? activeField?.value : undefined,
+  );
 
   const flatHostEventList = hostEventList?.pages.flatMap(
     (page) => page.data.content,
@@ -54,24 +68,22 @@ const UserEventScreen = () => {
     setActiveTabName(name);
   };
 
-  const handlePressTicket = (id: number) => {
-    // TODO: id 가지고 이동
+  const handlePressTicket = (eventInfoId: number) => {
     stackNavigation.navigate('UserTicket', {
-      eventId: 1,
+      eventId: eventInfoId,
     });
   };
 
-  // TODO
-  const handlePressHostEvent = (eventId: number) => {
-    // 1. 승인되지 않았을 경우, dialog 등장
-    // 2. 승인된 이벤트의 경우, id 가지고 이동
-    stackNavigation.navigate('HostConsole', { eventId });
+  const handlePressHostEvent = (event: HostEventInfoResponseDto) => {
+    if (event.isApproved) {
+      openDialog({
+        type: 'validate',
+        text: '아직 승인되지 않은 이벤트입니다.',
+      });
+      return;
+    }
+    stackNavigation.navigate('HostConsole', { eventId: event.eventInfoId });
   };
-
-  useEffect(() => {
-    // TODO: activeField가 변할 때, filter
-    // console.log(activeField?.label ?? '전체');
-  }, [activeField]);
 
   // TODO
   if (!ticketLists) {
@@ -106,25 +118,29 @@ const UserEventScreen = () => {
 
       {/* 참여 이벤트 */}
       {activeTabName === UserEventTabItem.PARTICIPANT &&
-        (ticketLists.length === 0 ? (
+        (flatUserTicketList?.length === 0 ? (
           <View style={userEventScreenStyles.emptyContainer}>
             <Text>{MENT_PARTICIPANT.MAIN.EMPTY}</Text>
           </View>
         ) : (
-          <ScrollView style={userEventScreenStyles.scrollContainer}>
-            <SpaceLayout size={15}>
-              {ticketLists.map((ticket) => (
+          <View style={userEventScreenStyles.scrollContainer}>
+            <FlatList
+              data={flatUserTicketList}
+              contentContainerStyle={userEventScreenStyles.flatListContentStyle}
+              ItemSeparatorComponent={ItemSeparatorComponent}
+              renderItem={({ item }) => (
                 <TicketList
-                  key={ticket.eventInfoId}
-                  eventTitle={ticket.eventTitle}
-                  eventDateList={ticket.eventDateList}
-                  fieldTypeList={ticket.fieldTypeList}
-                  onPress={() => handlePressTicket(ticket.eventInfoId)}
+                  key={item.eventInfoId}
+                  eventTitle={item.eventTitle}
+                  eventDateList={item.eventDateList}
+                  fieldTypeList={item.fieldTypeList}
+                  onPress={() => handlePressTicket(item.eventInfoId)}
                 />
-              ))}
-            </SpaceLayout>
-            <Spacing height={200} />
-          </ScrollView>
+              )}
+              onEndReachedThreshold={0.2}
+              onEndReached={onEndReached}
+            />
+          </View>
         ))}
 
       {/* 주최 이벤트 */}
@@ -147,7 +163,7 @@ const UserEventScreen = () => {
                     (eventIndexInfo) => eventIndexInfo.eventDate,
                   )}
                   fieldTypeList={item.fieldTypeList}
-                  onPress={() => handlePressHostEvent(item.eventInfoId)}
+                  onPress={() => handlePressHostEvent(item)}
                 />
               )}
               onEndReachedThreshold={0.2}
