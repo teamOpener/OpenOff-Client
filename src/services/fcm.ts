a5e93dca69825f0c91ced3035474dd0846181f19
Yeonju Jo <yjj0287@gmail.com>
2023-08-23 17:13:42 +0900

    🐛 fix: fcm refresh logic 제거

--- src/services/fcm.ts
+++ src/services/fcm.ts
@@ -1,44 +1,30 @@
 import notifee, { AndroidImportance } from '@notifee/react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
 import { permitAlert } from 'apis/user';
 import { Platform } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 import { PERMISSIONS } from 'react-native-permissions';
 import { useAuthorizeStore } from 'stores/Authorize';
-import AsyncAuthorizeStorage from 'types/apps/asyncAuthorizeStorage';
 import { requestSinglePermission } from './permission';
 
 const { setFcmToken } = useAuthorizeStore.getState();
 
 export const getToken = async () => {
   const deviceInfo = await DeviceInfo.getUniqueId();
-  const value = await AsyncStorage.getItem('authorize');
-  const authorizeStore: AsyncAuthorizeStorage = JSON.parse(value ?? '');
-  if (!authorizeStore.state.fcmToken) {
-    const fcmDeviceToken = await messaging().getToken();
-    await permitAlert({
-      fcmToken: fcmDeviceToken,
-      deviceId: deviceInfo,
-    });
-    setFcmToken(fcmDeviceToken);
-    return;
-  }
-  if (authorizeStore.state.fcmToken) {
-    messaging().onTokenRefresh(async (refreshFcmToken) => {
-      await permitAlert({
-        fcmToken: refreshFcmToken,
-        deviceId: deviceInfo,
-      });
-      setFcmToken(refreshFcmToken);
-    });
-  }
+  const fcmDeviceToken = await messaging().getToken();
+
+  await permitAlert({
+    fcmToken: fcmDeviceToken,
+    deviceId: deviceInfo,
+  });
+  setFcmToken(fcmDeviceToken);
 };
 
 // foreground alarm
 export const requestAlarmPermission = async () => {
   if (Platform.OS === 'ios') {
     const authorizationStatus = await messaging().requestPermission();
+
     const enabled =
       authorizationStatus === messaging.AuthorizationStatus.AUTHORIZED ||
       authorizationStatus === messaging.AuthorizationStatus.PROVISIONAL;
