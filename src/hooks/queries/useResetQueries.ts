9401682153c06508eb292801af188e51421f5dcb
Yeonju Jo <yjj0287@gmail.com>
2023-08-24 00:39:58 +0900

    🐛 fix: query key type 수정

--- src/hooks/queries/useResetQueries.ts
+++ src/hooks/queries/useResetQueries.ts
@@ -3,7 +3,7 @@ import { useQueryClient } from '@tanstack/react-query';
 const useResetQueries = () => {
   const queryClient = useQueryClient();
 
-  const resetQueries = (keys: Array<Array<string>>) => {
+  const resetQueries = (keys: Array<Array<string | number>>) => {
     if (!Array.isArray(keys)) {
       return;
     }
