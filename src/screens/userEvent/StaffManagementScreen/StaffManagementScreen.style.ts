9de979471166ce5717988c14361e22c44c394565
Yeonju Jo <yjj0287@gmail.com>
2023-08-23 14:31:17 +0900

    ✨ feat: 스태프 관리 페이지 제작

--- src/screens/userEvent/StaffManagementScreen/StaffManagementScreen.style.ts
+++ src/screens/userEvent/StaffManagementScreen/StaffManagementScreen.style.ts
@@ -1,7 +1,32 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextStyle } from 'react-native';
+import { colors, fonts, layouts } from 'styles/theme';
 
 const staffManagementScreenStyles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    paddingTop: layouts.PADDING,
+    paddingHorizontal: layouts.PADDING,
+  },
+  text: {
+    fontFamily: fonts.semibold,
+    fontSize: 17,
+    lineHeight: 17 * 1.4,
+  } as TextStyle,
+  hostWrapper: {
+    borderBottomColor: colors.main,
+    borderBottomWidth: 1,
+  },
+  infoText: {
+    fontFamily: fonts.semibold,
+    fontSize: 15,
+    lineHeight: 15 * 1.4,
+  } as TextStyle,
+  infoContainer: {
+    alignItems: 'center',
+  },
+  staffListContentContainer: {
+    gap: 16,
+  },
 });
 
 export default staffManagementScreenStyles;
--- src/screens/userEvent/StaffManagementScreen/StaffManagementScreen.tsx
+++ src/screens/userEvent/StaffManagementScreen/StaffManagementScreen.tsx
@@ -1,19 +1,72 @@
-import { useEffect } from 'react';
-import { View } from 'react-native';
+import dayjs from 'dayjs';
+import { ScrollView, View } from 'react-native';
 import Text from 'components/common/Text/Text';
 import { StackMenu } from 'constants/menu';
-import useRouteParams from 'hooks/navigator/useRouteParams';
+import useStackRoute from 'hooks/navigator/useStackRoute';
+import { useLedgerStatus, useStaffLists } from 'hooks/queries/ledger';
+import StaffType from 'models/ledger/entity/StaffType';
+import SpaceLayout from 'components/layout/Space/SpaceLayout';
+import Spacing from 'components/common/Spacing/Spacing';
+import { StaffList } from 'components/userEvent/host';
+import Icon from 'components/common/Icon/Icon';
+import staffManagementScreenStyles from './StaffManagementScreen.style';
 
 const StaffManagementScreen = () => {
-  const params = useRouteParams<StackMenu.StaffManagement>();
+  const { params } = useStackRoute<StackMenu.StaffManagement>();
 
-  useEffect(() => {
-    console.log(params?.eventInfoId);
-  }, [params]);
+  const { data: eventInfo } = useLedgerStatus(params.eventIndex);
+
+  const { data: staffList } = useStaffLists({
+    eventInfoId: params.eventId,
+  });
+  const host = staffList?.find((staff) => staff.staffType === StaffType.M);
+  const subStaff = staffList?.filter(
+    (staff) => staff.staffType === StaffType.S,
+  );
 
   return (
-    <View>
-      <Text>스태프 관리</Text>
+    <View style={staffManagementScreenStyles.container}>
+      <SpaceLayout
+        direction="row"
+        size={10}
+        style={staffManagementScreenStyles.infoContainer}
+      >
+        <Icon name="IconCalendar" size={15} fill="white" />
+        <Text style={staffManagementScreenStyles.infoText}>
+          {dayjs(eventInfo?.eventDate).format('M/D(ddd) HH:MM')}
+        </Text>
+      </SpaceLayout>
+
+      <Spacing height={30} />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <SpaceLayout size={10}>
+          <Text style={staffManagementScreenStyles.text}>주최자</Text>
+          <View style={staffManagementScreenStyles.hostWrapper}>
+            <Text style={staffManagementScreenStyles.text} color="main">
+              {host?.staffName ?? ''}
+            </Text>
+          </View>
+        </SpaceLayout>
+
+        <Spacing height={30} />
+
+        <Text style={staffManagementScreenStyles.text}>스태프</Text>
+        <Spacing height={10} />
+
+        <SpaceLayout size={16}>
+          {subStaff?.map((staff) => (
+            <StaffList
+              key={staff.userId}
+              nickName={staff.staffName}
+              mode="minus"
+              eventInfoId={params.eventId}
+            />
+          ))}
+
+          <StaffList mode="plus" eventInfoId={params.eventId} />
+        </SpaceLayout>
+      </ScrollView>
     </View>
   );
 };
