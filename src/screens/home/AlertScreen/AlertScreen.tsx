f962f4c693e073175805924e36cd1b88bdd4432a
Yeonju Jo <yjj0287@gmail.com>
2023-08-25 03:03:03 +0900

    🗑️ remove: 필요없는 파일 삭제

--- src/screens/home/AlertScreen/AlertScreen.tsx
+++ src/screens/home/AlertScreen/AlertScreen.tsx
@@ -1,14 +1,13 @@
 import AlertCard from 'components/home/cards/AlertCard/AlertCard';
-import alertList from 'mocks/lists/alertList';
 import { ScrollView } from 'react-native';
 import alertScreenStyles from './AlertScreen.style';
 
 const AlertScreen = () => {
   return (
     <ScrollView style={alertScreenStyles.container}>
-      {alertList.map((alert, _id) => (
+      {/* {alertList.map((alert, _id) => (
         <AlertCard key={_id} alert={alert} />
-      ))}
+      ))} */}
     </ScrollView>
   );
 };
