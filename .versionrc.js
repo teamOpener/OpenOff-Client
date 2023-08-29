9c97c0ffc34fd1ea8400663ec7b1aaa272d00e0f
Yeonju Jo <yjj0287@gmail.com>
2023-08-29 16:43:05 +0900

    👷 ci: changelog template 변경

--- .versionrc.js
+++ .versionrc.js
@@ -1,8 +1,8 @@
 module.exports = {
+  header: '# 🌈 Changelog',
   types: [
     { type: 'feat', section: '✨ Features', hidden: false },
     { type: 'fix', section: '🐛 Bug Fixes', hidden: false },
-    { type: 'chore', section: '🔧 Others', hidden: false },
     {
       type: 'improvement',
       section: '💫 Feature Improvements',
@@ -10,8 +10,9 @@ module.exports = {
     },
     { type: 'design', section: '💅 Design', hidden: false },
     { type: 'refactor', section: '🫧 Code Refactoring', hidden: false },
-    { type: 'test', section: '✅ Tests', hidden: false },
-    { type: 'ci', section: '👷 CI', hidden: false },
+    { type: 'chore', hidden: true },
+    { type: 'test', hidden: true },
+    { type: 'ci', hidden: true },
   ],
   releaseCommitMessageFormat: '⛴️ chore(release): v{{currentTag}}',
 };
