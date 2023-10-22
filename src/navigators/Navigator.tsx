e966b77228fec897478661ad3500336ca51075f9
HoCG <rhwkd5012@naver.com>
2023-10-22 22:37:43 +0900

    ✨ feat: 네비게이터 구조 개선

--- src/navigators/Navigator.tsx
+++ src/navigators/Navigator.tsx
@@ -1,20 +1,23 @@
 import CommonSuspense from 'components/suspense/loading/CommonSuspense/CommonSuspense';
-import { useAuthorizeStore } from 'stores/Authorize';
-import AuthorizeNavigator from './AuthorizeNavigator';
 import StackNavigator from './StackNavigator';
 
 const Navigator = () => {
-  const { isLogin } = useAuthorizeStore();
-
-  return isLogin ? (
+  return (
     <CommonSuspense>
       <StackNavigator />
     </CommonSuspense>
-  ) : (
-    <CommonSuspense>
-      <AuthorizeNavigator />
-    </CommonSuspense>
   );
+  // const { isLogin } = useAuthorizeStore();
+
+  // return isLogin ? (
+  //   <CommonSuspense>
+  //     <StackNavigator />
+  //   </CommonSuspense>
+  // ) : (
+  //   <CommonSuspense>
+  //     <AuthorizeNavigator />
+  //   </CommonSuspense>
+  // );
 };
 
 export default Navigator;
--- src/navigators/StackNavigator.tsx
+++ src/navigators/StackNavigator.tsx
@@ -5,7 +5,7 @@ import {
 } from '@react-navigation/stack';
 import BackButton from 'components/navigator/BackButton';
 import ExitButton from 'components/navigator/ExitButton/ExitButton';
-import { StackMenu } from 'constants/app/menu';
+import { AuthorizeMenu, StackMenu } from 'constants/app/menu';
 import MENT_NAVIGATOR from 'constants/navigator/navigatorMessage';
 import resetQueryKeys from 'constants/queries/resetQueryKey';
 import useRefetchOnFocus from 'hooks/app/useRefetchOnFocus';
@@ -35,9 +35,55 @@ import StaffManagementScreen from 'screens/userEvent/StaffManagementScreen/Staff
 import UserQRScreen from 'screens/userEvent/UserQRScreen/UserQRScreen';
 import UserTicketScreen from 'screens/userEvent/UserTicketScreen/UserTicketScreen';
 import { colors, fonts } from 'styles/theme';
+import NicknameScreen from 'screens/authorize/joins/NickNameScreen/NickNameScreen';
+import EmailPasswordFindScreen from 'screens/authorize/finds/EmailPasswordFindScreen/EmailPasswordFindScreen';
+import JoinCompleteScreen from 'screens/authorize/joins/JoinCompleteScreen/JoinCompleteScreen';
+import BackToHomeButton from 'components/authorize/buttons/BackToHomeButton/BackToHomeButton';
+import InterestFieldScreen from 'screens/authorize/joins/InterestFieldScreen/InterestFieldScreen';
+import UserInfoScreen from 'screens/authorize/joins/UserInfoScreen/UserInfoScreen';
+import LoginScreen from 'screens/authorize/logins/LoginScreen/LoginScreen';
+import EmailPasswordScreen from 'screens/authorize/joins/EmailPasswordScreen/EmailPasswordScreen';
+import AgreeToTermScreen from 'screens/authorize/joins/AgreeToTermScreen/AgreeToTermScreen';
+import PhoneCertificationScreen from 'screens/authorize/joins/PhoneCertificationScreen/PhoneCertificationScreen';
+import textStyles from 'styles/textStyles';
+import { GenderType, UserInfoStatus } from 'constants/authorize/join';
+import { Action, JoinInfo } from 'types/join';
+import { Reducer, useReducer } from 'react';
 import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createStackNavigator();
+
+const userInfoReducer = (state: JoinInfo, action: Action): JoinInfo => {
+  switch (action.type) {
+    case UserInfoStatus.SET_NAME:
+      return { ...state, username: action.username };
+    case UserInfoStatus.SET_BIRTH:
+      return { ...state, birth: action.birth };
+    case UserInfoStatus.SET_AGREE_TO_TERM:
+      return { ...state, agreeToTerm: action.term };
+    case UserInfoStatus.SET_PHONE_NUMBER:
+      return { ...state, phoneNumber: action.phoneNumber };
+    case UserInfoStatus.SET_GENDER:
+      return { ...state, gender: action.gender };
+    case UserInfoStatus.SET_NICK_NAME:
+      return { ...state, nickname: action.nickname };
+    case UserInfoStatus.SET_EMAIL_ADDRESS_PASSWORD:
+      return {
+        ...state,
+        emailAddress: action.emailPassword.email,
+        password: action.emailPassword.password,
+      };
+    case UserInfoStatus.SET_INTEREST_FIELD:
+      return { ...state, interestField: action.interestField };
+    case UserInfoStatus.SET_ACCOUNT_TYPE:
+      return {
+        ...state,
+        accountType: action.accountType,
+      };
+    default:
+      return state;
+  }
+};
 
 const StackNavigator = () => {
   const defaultOptions: StackNavigationOptions = {
@@ -54,6 +100,24 @@ const StackNavigator = () => {
     },
     headerLeft: BackButton,
   };
+
+  const initialState: JoinInfo = {
+    username: '',
+    birth: '',
+    agreeToTerm: '',
+    phoneNumber: '',
+    nickname: '',
+    gender: GenderType.MAN,
+    emailAddress: '',
+    password: '',
+    interestField: [],
+    accountType: 'NORMAL',
+  };
+
+  const [state, dispatch] = useReducer<Reducer<JoinInfo, Action>>(
+    userInfoReducer,
+    initialState,
+  );
 
   const noTextOptions: StackNavigationOptions = {
     ...defaultOptions,
@@ -292,6 +356,110 @@ const StackNavigator = () => {
         name={StackMenu.Scrap}
         component={ScrapScreen}
       />
+      {/** authorize */}
+      <Stack.Screen
+        options={{
+          headerTitle: '',
+          headerLeft: BackButton,
+        }}
+        name={AuthorizeMenu.Login}
+      >
+        {() => <LoginScreen state={state} dispatch={dispatch} />}
+      </Stack.Screen>
+      <Stack.Screen
+        options={{
+          headerTitle: '',
+          headerLeft: BackButton,
+        }}
+        name={AuthorizeMenu.EmailPassword}
+      >
+        {() => <EmailPasswordScreen dispatch={dispatch} />}
+      </Stack.Screen>
+      <Stack.Screen
+        options={{
+          headerTitle: '',
+          headerLeft: () => null,
+        }}
+        name={AuthorizeMenu.AgreeToTerm}
+      >
+        {() => <AgreeToTermScreen state={state} dispatch={dispatch} />}
+      </Stack.Screen>
+      <Stack.Screen
+        options={{
+          headerTitle: '',
+          headerLeft: BackButton,
+        }}
+        name={AuthorizeMenu.PhoneCertification}
+      >
+        {() => <PhoneCertificationScreen dispatch={dispatch} />}
+      </Stack.Screen>
+      <Stack.Screen
+        options={{
+          headerTitle: '',
+          headerLeft: BackButton,
+        }}
+        name={AuthorizeMenu.Nickname}
+      >
+        {() => <NicknameScreen dispatch={dispatch} />}
+      </Stack.Screen>
+      <Stack.Screen
+        options={{
+          headerTitle: '',
+          headerLeft: BackButton,
+        }}
+        name={AuthorizeMenu.UserInfo}
+      >
+        {() => <UserInfoScreen state={state} dispatch={dispatch} />}
+      </Stack.Screen>
+      <Stack.Screen
+        options={{
+          headerTitle: '',
+          headerLeft: BackButton,
+        }}
+        name={AuthorizeMenu.InterestField}
+      >
+        {() => <InterestFieldScreen state={state} dispatch={dispatch} />}
+      </Stack.Screen>
+      <Stack.Screen
+        options={{
+          headerTitle: '',
+          // eslint-disable-next-line react/no-unstable-nested-components
+          headerLeft: () => <BackToHomeButton />,
+        }}
+        name={AuthorizeMenu.JoinComplete}
+      >
+        {() => <JoinCompleteScreen state={state} />}
+      </Stack.Screen>
+      <Stack.Screen
+        name={AuthorizeMenu.EmailPasswordFind}
+        options={{
+          headerLeft: BackButton,
+          headerStyle: {
+            backgroundColor: colors.background,
+            ...Platform.select({
+              ios: {
+                shadowColor: colors.background,
+                shadowOffset: {
+                  width: 0,
+                  height: 0,
+                },
+                shadowOpacity: 0.2,
+              },
+              android: {
+                elevation: 0,
+              },
+            }),
+          },
+          headerTitleAlign: 'center',
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            ...textStyles.h3,
+            color: colors.white,
+          },
+        }}
+      >
+        {() => <EmailPasswordFindScreen />}
+      </Stack.Screen>
     </Stack.Navigator>
   );
 };
