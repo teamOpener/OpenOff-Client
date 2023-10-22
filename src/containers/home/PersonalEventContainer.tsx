7a293d8897d15a9e2f7835db7242a39c1f7e4186
HoCG <rhwkd5012@naver.com>
2023-10-22 22:33:16 +0900

    ✨ feat: 맞춤이벤트 카드 컨테이너화

---
+++ src/containers/home/PersonalEventContainer.tsx
@@ -0,0 +1,33 @@
+import Spacing from 'components/common/Spacing/Spacing';
+import EventCardList from 'components/home/lists/EventCardList/EventCardList';
+import useInterestFields from 'hooks/interest/useInterestFields';
+import { usePersonalEventLists } from 'hooks/queries/event';
+import { useMyInfo } from 'hooks/queries/user';
+import i18n from 'locales';
+
+const PersonalEventContainer = () => {
+  const { data: personalEventLists, isLoading: isPersonalLoading } =
+    usePersonalEventLists();
+
+  const { data: userInfo } = useMyInfo();
+
+  const { generateInterestFieldTags } = useInterestFields();
+
+  const userInterest = userInfo?.userInfo.fieldTypeList.map((field) => {
+    return `#${
+      generateInterestFieldTags().find(
+        (fieldElement) => fieldElement.value === field,
+      )?.label
+    }   `;
+  });
+  return (
+    <EventCardList
+      isLoading={isPersonalLoading}
+      events={personalEventLists}
+      title={i18n.t('personal_event_commend')}
+      subTitle={userInterest?.join('') ?? ''}
+    />
+  );
+};
+
+export default PersonalEventContainer;
