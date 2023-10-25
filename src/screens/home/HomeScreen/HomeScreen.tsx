e44daa552c693a966ff234f87fad1eb3428419ae
HoCG <rhwkd5012@naver.com>
2023-10-25 21:58:20 +0900

    ✨ feat: 맞춤 이벤트에 로그인 조건 추가

--- src/screens/home/HomeScreen/HomeScreen.tsx
+++ src/screens/home/HomeScreen/HomeScreen.tsx
@@ -6,40 +6,42 @@ import FloatingButton from 'components/home/floatingbutton/FloatingButton';
 import CategoryButtonGroup from 'components/home/groups/CategoryButtonGroup/CategoryButtonGroup';
 import EventCardList from 'components/home/lists/EventCardList/EventCardList';
 import { StackMenu } from 'constants/app/menu';
-import useInterestFields from 'hooks/interest/useInterestFields';
 import useNavigator from 'hooks/navigator/useNavigator';
-import { usePersonalEventLists, useVogueEventLists } from 'hooks/queries/event';
+import { useVogueEventLists } from 'hooks/queries/event';
 import useResetQueries from 'hooks/queries/useResetQueries';
-import { useMyInfo } from 'hooks/queries/user';
 import { useCallback, useEffect } from 'react';
 import { Image, ScrollView, TouchableOpacity, View } from 'react-native';
 import { foregroundListener, requestAlarmPermission } from 'services/fcm';
+import { useAuthorizeStore } from 'stores/Authorize';
+import PersonalEventContainer from 'containers/home/PersonalEventContainer';
+import useDialog from 'hooks/app/useDialog';
 import homeScreenStyles from './HomeScreen.style';
 
 const HomeScreen = () => {
+  const { isLogin } = useAuthorizeStore();
+  const { openDialog } = useDialog();
   const { stackNavigation } = useNavigator();
 
   const { data: vogueEventLists, isLoading: isVogueLoading } =
     useVogueEventLists();
-  const { data: personalEventLists, isLoading: isPersonalLoading } =
-    usePersonalEventLists();
-  const { data: userInfo } = useMyInfo();
-
-  const { generateInterestFieldTags } = useInterestFields();
-
-  const userInterest = userInfo?.userInfo.fieldTypeList.map((field) => {
-    return `#${
-      generateInterestFieldTags().find(
-        (fieldElement) => fieldElement.value === field,
-      )?.label
-    }   `;
-  });
 
   const handleCategoryPress = (value: string) => {
     stackNavigation.navigate(StackMenu.CategoryEvent, { fieldValue: value });
   };
 
   const handleShowBookmarkEvent = () => {
+    if (!isLogin) {
+      openDialog({
+        type: 'confirm',
+        text: i18n.t('need_to_login'),
+        apply: () => {
+          stackNavigation.navigate('Login');
+        },
+        applyText: i18n.t('yes'),
+        closeText: i18n.t('no'),
+      });
+      return;
+    }
     stackNavigation.navigate(StackMenu.BookmarkEvent);
   };
 
@@ -84,15 +86,8 @@ const HomeScreen = () => {
         </View>
         <AdvertisementCarousel />
         <CategoryButtonGroup handlePress={handleCategoryPress} />
-
         <Spacing height={20} />
-
-        <EventCardList
-          isLoading={isPersonalLoading}
-          events={personalEventLists}
-          title={i18n.t('personal_event_commend')}
-          subTitle={userInterest?.join('') ?? ''}
-        />
+        {isLogin && <PersonalEventContainer />}
         <EventCardList
           isLoading={isVogueLoading}
           events={vogueEventLists?.content}
